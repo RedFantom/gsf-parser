9f284d72c10655ead600f236f3ba237b9125fc84
RedFantom <redfantom@outlook.com>
2016-11-30 13:36:18 +0100

    Added error message for Unix

--- main.py
+++ main.py
@@ -2,6 +2,11 @@
 # Thranta Squadron GSF CombatLog Parser, Copyright (C) 2016 by RedFantom and Daethyra
 # For license see LICENSE
 import gui
+import sys
+import tkMessageBox
 
 if __name__ == "__main__":
-    main_window = gui.main_window()
+    if sys.platform == "win32":
+        main_window = gui.main_window()
+    else:
+        tkMessageBox.showerror("Fatal error", "Unix is not supported")
