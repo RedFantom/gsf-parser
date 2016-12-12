ae6251e447c8ea3ab3ee4ea4f44a06b8ad1c7604
RedFantom <redfantom@outlook.com>
2016-12-12 16:39:40 +0100

    Implemented position and opacity setting for overlay

--- overlay.py
+++ overlay.py
@@ -4,13 +4,14 @@
 
 # UI imports
 import Tkinter as tk
-import tkMessageBox
 import ttk
+import tkMessageBox
 # General imports
 import os
 # Own modules
 import vars
 import frames
+import main
 
 class splash_screen(tk.Toplevel):
     def __init__(self, window, boot=False):
@@ -19,12 +20,16 @@ class splash_screen(tk.Toplevel):
         self.label.pack()
         self.progress_bar = ttk.Progressbar(self, orient = "horizontal", length = 300, mode = "determinate")
         self.progress_bar.pack()
-        list = os.listdir(window.default_path)
+        try:
+            list = os.listdir(window.default_path)
+        except:
+            print "[DEBUG] Running on UNIX, functionality disabled"
+            return
         files = []
         for file in list:
             if file.endswith(".txt"):
-                files.append(file)    
-        vars.files_done = 0           
+                files.append(file)
+        vars.files_done = 0
         self.amount_files = len(files)
         if(self.amount_files >= 50 and boot):
             tkMessageBox.showinfo("Notice", "You currently have more than 50 CombatLogs in your CombatLogs folder. You may want to archive some of your %s CombatLogs in order to speed up the parsing program and the startup times." % self.amount_files)
@@ -40,12 +45,30 @@ class overlay(tk.Toplevel):
     def __init__(self, window):
         tk.Toplevel.__init__(self, window)
         self.attributes("-topmost", True)
-        self.attributes("-alpha", 0.7)
+        self.attributes("-alpha", main.set_obj.opacity)
         self.overrideredirect(True)
-        self.wm_geometry("200x75+0+0")
+        if main.set_obj.pos == "TL":
+            pos_c = "+0+0"
+        elif main.set_obj.pos == "BL":
+            pos_c = "+0+" + str(vars.screen_h - 75)
+        elif main.set_obj.pos == "TR":
+            if main.set_obj.size == "big":
+                pos_c = "+" + str(vars.screen_w - 200) + "+0"
+            elif main.set_obj.size == "small":
+                pos_c = "+" + str(vars.screen_w - 100) + "+0"
+        elif main.set_obj.pos == "BR":
+            if main.set_obj.size == "big":
+                pos_c = "+" + str(vars.screen_w - 200) + "+" + str(vars.screen_h - 75)
+            elif main.set_obj.size == "small":
+                pos_c = "+" + str(vars.screen_w - 100) + "+" + str(vars.screen_h - 75)                
+        if main.set_obj.size == "big":
+            self.wm_geometry("200x75" + pos_c)
+            self.text_label = tk.Label(self, text = "Damage done:\nDamage taken:\nHealing recv:\nSelfdamage:\n", justify = tk.LEFT)
+        elif main.set_obj.size == "small":
+            self.wm_geometry("100x75" + pos_c)
+            self.text_label = tk.label(self, text = "DD:\nDT:\nHR:\nSD:\n", justify = tk.LEFT)
         self.stats_var = tk.StringVar()
         self.stats_label = tk.Label(self, textvariable = self.stats_var, justify = tk.RIGHT)
-        self.text_label = tk.Label(self, text = "Damage done:\nDamage taken:\nHealing recv:\nSelfdamage:\n", justify = tk.LEFT)
         # self.stats_label.config(font=("Courier", 44))
         self.text_label.pack(side=tk.LEFT)
         self.stats_label.pack(side=tk.RIGHT)
@@ -56,4 +79,3 @@ class overlay(tk.Toplevel):
 class privacy(tk.Toplevel):
     def __init__(self, window):
         tk.Toplevel.__init__(self, window)
-        
