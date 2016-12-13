7952f98cbc0eb87aa4df27024370d1c17b1ca29d
RedFantom <redfantom@outlook.com>
2016-12-13 17:54:37 +0100

    Replaced a pickle with a shelve

--- settings.py
+++ settings.py
@@ -5,31 +5,30 @@ import vars
 import tkMessageBox
 import getpass
 import os
-import cPickle
+import shelve
 
 class defaults:
     version = "2.0.0_alpha"
     cl_path = 'C:/Users/' + getpass.getuser() + "/Documents/Star Wars - The Old Republic/CombatLogs"
-    auto_ident = str(False)
+    auto_ident = False
     server = ("thrantasquadron.tk", 83)
-    auto_upl = str(False)
-    overlay = str(True)
-    opacity = str(0.7)
+    auto_upl = False
+    overlay = True
+    opacity = 0.7
     size = "big"
     pos = "TL"
 
-class settings:
-    def __init__(self, file_name = "settings.ini"):
+class settings():
+    def __init__(self, file_name = "settings.db"):
         self.file_name = file_name
-
         vars.install_path = os.getcwd()
         if self.file_name not in os.listdir(vars.install_path):
             print "[DEBUG] Settings file could not be found. Creating a new file with default settings"
-            self.write_def()
+            # self.write_def()
+        self.read_set()
 
     def read_set(self):
-        with open(self.file_name, "r") as settings_file_object:
-            settings_dict = cPickle.load(settings_file_object)
+        settings_dict = shelve.open(self.file_name)
         self.version = settings_dict["version"]
         self.cl_path = settings_dict["cl_path"]
         self.auto_ident = settings_dict["auto_ident"]
@@ -39,35 +38,38 @@ class settings:
         self.opacity = settings_dict["opacity"]
         self.size = settings_dict["size"]
         self.pos = settings_dict["pos"]
+        settings_dict.sync()
+        settings_dict.close()
 
     def write_def(self):
-        settings_dict = {"version":defaults.version,
-                         "cl_path":defaults.cl_path,
-                         "auto_ident":bool(defaults.auto_ident),
-                         "server":defaults.server,
-                         "auto_upl":bool(defaults.auto_upl),
-                         "overlay":bool(defaults.overlay),
-                         "opacity":float(defaults.opacity),
-                         "size":defaults.size,
-                         "pos":defaults.pos
-                        }
-        with open(self.file_name, "w") as settings_file:
-            cPickle.dump(settings_dict, settings_file)
+        settings_dict = shelve.open(self.file_name)
+        settings_dict["version"] = defaults.version
+        settings_dict["cl_path"] = defaults.cl_path
+        settings_dict["auto_ident"] = defaults.auto_ident
+        settings_dict["server"] = defaults.server
+        settings_dict["auto_upl"] = defaults.auto_upl
+        settings_dict["overlay"] = defaults.overlay
+        settings_dict["opacity"] = defaults.opacity
+        settings_dict["size"] = defaults.size
+        settings_dict["pos"] = defaults.pos
+        settings_dict.sync()
+        settings_dict.close()
 
     def write_set(self, version=defaults.version, cl_path=defaults.cl_path, 
                   auto_ident=defaults.auto_ident, server=defaults.server, 
                   auto_upl=defaults.auto_upl, overlay=defaults.overlay, 
                   opacity=defaults.opacity, size=defaults.size, pos=defaults.pos):
-        settings_dict = {"version":version,
-                         "cl_path":cl_path,
-                         "auto_ident":bool(auto_ident),
-                         "server":server,
-                         "auto_upl":bool(auto_upl),
-                         "overlay":bool(overlay),
-                         "opacity":float(opacity),
-                         "size":str(size),
-                         "pos":pos
-                        }
-        with open(self.file_name, "w") as settings_file_object:
-            cPickle.dump(settings_dict, settings_file_object)
+        settings_dict = shelve.open(self.file_name)
+        settings_dict["version"] = version
+        settings_dict["cl_path"] = cl_path
+        settings_dict["auto_ident"] = auto_ident
+        settings_dict["server"] = server
+        settings_dict["auto_upl"] = auto_upl
+        settings_dict["overlay"] = overlay
+        settings_dict["opacity"] = opacity
+        settings_dict["size"] = size
+        settings_dict["pos"] = pos
+        print settings_dict
+        settings_dict.sync()
+        settings_dict.close()
         self.read_set()
