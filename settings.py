0d8cce7205c6c449f9a3957e3eb94f90fd0c6b03
RedFantom <redfantom@outlook.com>
2016-12-14 15:24:20 +0100

    Added commenting to settings.py

--- settings.py
+++ settings.py
@@ -1,32 +1,57 @@
 # Written by RedFantom, Wing Commander of Thranta Squadron and Daethyra, Squadron Leader of Thranta Squadron
 # Thranta Squadron GSF CombatLog Parser, Copyright (C) 2016 by RedFantom and Daethyra
 # For license see LICENSE
-import vars
+
+# UI imports
 import tkMessageBox
+# General imports
 import getpass
 import os
 import cPickle
+# Own modules
+import vars
 
+# Class with default settings for in the settings file
 class defaults:
+    # Version to display in settings tab
     version = "2.0.0_alpha"
+    # Path to get the CombatLogs from
     cl_path = 'C:/Users/' + getpass.getuser() + "/Documents/Star Wars - The Old Republic/CombatLogs"
+    # Automatically send and retrieve names and hashes of ID numbers from the remote server
     auto_ident = str(False)
+    # Address and port of the remote server
     server = ("thrantasquadron.tk", 83)
+    # Automatically upload CombatLogs as they are parsed to the remote server
     auto_upl = str(False)
+    # Enable the overlay
     overlay = str(True)
+    # Set the overlay opacity, or transparency
     opacity = str(0.7)
+    # Set the overlay size
     size = "big"
+    # Set the corner the overlay will be displayed in
     pos = "TL"
 
+
+# Class that loads, stores and saves settings
 class settings:
+    # Set the file_name for use by other functions
     def __init__(self, file_name = "settings.ini"):
         self.file_name = file_name
-
+        # Set the install path in the vars module
         vars.install_path = os.getcwd()
+        # Check for the existence of the specified settings_file
         if self.file_name not in os.listdir(vars.install_path):
             print "[DEBUG] Settings file could not be found. Creating a new file with default settings"
             self.write_def()
+        else:
+            try:
+                self.read_set()
+            except:
+                tkMessageBox.showerror("Error", "Settings file available, but it could not be read. Writing defaults.")
+                self.write_def()
 
+    # Read the settings from a file containing a pickle and store them as class variables
     def read_set(self):
         with open(self.file_name, "r") as settings_file_object:
             settings_dict = cPickle.load(settings_file_object)
@@ -40,6 +65,7 @@ class settings:
         self.size = settings_dict["size"]
         self.pos = settings_dict["pos"]
 
+    # Write the defaults settings found in the class defaults to a pickle in a file
     def write_def(self):
         settings_dict = {"version":defaults.version,
                          "cl_path":defaults.cl_path,
@@ -54,6 +80,8 @@ class settings:
         with open(self.file_name, "w") as settings_file:
             cPickle.dump(settings_dict, settings_file)
 
+    # Write the settings passed as arguments to a pickle in a file
+    # Setting defaults to default if not specified, so all settings are always written
     def write_set(self, version=defaults.version, cl_path=defaults.cl_path,
                   auto_ident=defaults.auto_ident, server=defaults.server,
                   auto_upl=defaults.auto_upl, overlay=defaults.overlay,
