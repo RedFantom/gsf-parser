21eb1b68883491d9f6dbaf02b284d41095c7be15
RedFantom <redfantom@outlook.com>
2016-12-21 20:11:23 +0100

    Added various Toplevels for a nicer UI

--- overlay.py
+++ overlay.py
@@ -3,7 +3,8 @@
 # For license see LICENSE
 
 # UI imports
-import Tkinter as tk
+import mtTkinter as tk
+from PIL import ImageTk, Image
 import ttk
 import tkMessageBox
 # General imports
@@ -12,14 +13,11 @@ import getpass
 import sys
 # Own modules
 import vars
+import statistics
 
 class splash_screen(tk.Toplevel):
     def __init__(self, window, boot=False):
         tk.Toplevel.__init__(self, window)
-        if sys.platform == "win32":
-            with open('C:/Users/' + getpass.getuser() + "/AppData/Local/SWTOR/swtor/settings/client_settings.ini", "r") as swtor:
-                if "D3DFullScreen = true" in swtor:
-                    tkMessageBox.showerror("Error", "The overlay cannot be shown with the current SWTOR settings. Please set SWTOR to Fullscreen (windowed) in the Grapichs settings.")
         self.label = ttk.Label(self, text = "Working...")
         self.label.pack()
         self.progress_bar = ttk.Progressbar(self, orient = "horizontal", length = 300, mode = "determinate")
@@ -48,6 +46,10 @@ class splash_screen(tk.Toplevel):
 class overlay(tk.Toplevel):
     def __init__(self, window):
         tk.Toplevel.__init__(self, window)
+        if sys.platform == "win32":
+            with open('C:/Users/' + getpass.getuser() + "/AppData/Local/SWTOR/swtor/settings/client_settings.ini", "r") as swtor:
+                if "D3DFullScreen = true" in swtor:
+                    tkMessageBox.showerror("Error", "The overlay cannot be shown with the current SWTOR settings. Please set SWTOR to Fullscreen (windowed) in the Graphics settings.")
         if vars.set_obj.pos == "TL":
             pos_c = "+0+0"
         elif vars.set_obj.pos == "BL":
@@ -104,9 +106,86 @@ class privacy(tk.Toplevel):
         try:
             privacy_list = list(privacy)
         except:
-            raise
+            tkMessageBox.showerror("Error", "Data in privacy statement received is not valid.")
             return
         index = 0
         for line in privacy_list:
             privacy_listbox.insert(0, line)
             index += 1
+
+class boot_splash(tk.Toplevel):
+    def __init__(self, window):
+        tk.Toplevel.__init__(self, window)
+        try:
+            self.logo = ImageTk.PhotoImage(Image.open(os.path.dirname(os.path.realpath(__file__)) + "\\logo.png"))
+            self.panel = ttk.Label(self, image = self.logo)
+            self.panel.pack()
+        except:
+            print "[DEBUG] No logo.png found in the home folder."
+        self.window = window
+        self.label_var = tk.StringVar()
+        self.label_var.set("Connecting to specified server...")
+        self.label = ttk.Label(self, textvariable = self.label_var)
+        self.label.pack()
+        self.progress_bar = ttk.Progressbar(self, orient = "horizontal", length = 462, mode = "determinate")
+        self.progress_bar.pack()
+        list = os.listdir(window.default_path)
+        files = []
+        for file in list:
+            if file.endswith(".txt"):
+                files.append(file)
+        vars.files_done = 0
+        self.amount_files = len(files)
+        '''
+        if self.amount_files >= 50:
+            tkMessageBox.showinfo("Notice", "You currently have more than 50 CombatLogs in your CombadwLogs folder. You may want to archive some of your %s CombatLogs in order to speed up the parsing program and the startup times." % self.amount_files)
+        '''
+        self.progress_bar["maximum"] = self.amount_files
+        self.progress_bar["value"] = 0
+        self.update()
+        self.done = False
+
+    def update_progress(self):
+        if vars.files_done != self.amount_files:
+            self.label_var.set("Parsing the files...")
+            self.progress_bar["value"] = vars.files_done
+            self.update()
+            self.window.after(500, self.update_progress)
+        else:
+            return
+
+class conn_splash(tk.Toplevel):
+    def __init__(self, window=vars.main_window):
+        tk.Toplevel.__init__(self, window)
+        self.FLAG = False
+        self.label = ttk.Label(self, text = "Connecting to specified server...")
+        self.label.pack()
+        self.conn_bar =  ttk.Progressbar(self, orient = "horizontal", length = 300, mode = "indeterminate")
+        self.conn_bar.pack()
+        self.window.after(500, self.connect)
+
+    def connect(self):
+        if not self.FLAG:
+            self.update()
+            self.window.after(500, self.connect)
+        else:
+            return
+
+
+class events_view(tk.Toplevel):
+    def __init__(self, window, spawn, player):
+        tk.Toplevel.__init__(self, window)
+        self.listbox = tk.Listbox(self, width=105, height=15, font=("Consolas", 10))
+        self.scroll = ttk.Scrollbar(self, orient=tk.VERTICAL, command =self.listbox.yview)
+        self.listbox.config(yscrollcommand=self.scroll.set)
+        self.listbox.grid(column = 1, row = 0, sticky=tk.N+tk.S+tk.W+tk.E)
+        self.scroll.grid(column = 2, row = 0, sticky=tk.N+tk.S+tk.W+tk.E)
+        self.resizable(width = False, height = False)
+        try:
+            for line in spawn:
+                self.listbox.insert(tk.END, statistics.print_event(line, vars.match_timing, player))
+        except TypeError:
+            self.destroy()
+
+
+
