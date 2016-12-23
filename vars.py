0c0e5a488ba143cadcbb045ee9ea0872f88a1fbf
RedFantom <redfantom@outlook.com>
2016-12-23 15:42:18 +0100

    Removed duplicate entries

--- vars.py
+++ vars.py
@@ -70,11 +70,9 @@ client_obj = None
 main_window = None
 cl_path = None
 
-spawn_timings = None
+spawn_timing = None
 spawn_index = None
 spawn = None
-match_timing = None
-match_timings = None
 rt_timing = None
 
 needs_closing = []
