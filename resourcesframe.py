f687489703faaf906bba310a9a8ae8e2d91a6969
RedFantom <redfantom@outlook.com>
2017-01-23 18:00:42 +0100

    Added License and Copyright notice

--- resourcesframe.py
+++ resourcesframe.py
@@ -1,3 +1,6 @@
+# Written by RedFantom, Wing Commander of Thranta Squadron and Daethyra, Squadron Leader of Thranta Squadron
+# Thranta Squadron GSF CombatLog Parser, Copyright (C) 2016 by RedFantom and Daethyra
+# For license see LICENSE
 import Tkinter as tk
 import ttk
 import webbrowser
