1daea449d5f2291997ec55417a7d2a472c4732e1
RedFantom <redfantom@outlook.com>
2017-02-01 15:25:12 +0100

    Added docstring

--- sharingframe.py
+++ sharingframe.py
@@ -6,6 +6,13 @@ import ttk
 import Tkinter as tk
 
 class share_frame(ttk.Frame):
+    """
+    A Frame to contain widgets to allow uploading of CombatLogs to the server
+    and viewing leaderboards that keep track of individual player performance
+    on different fronts. A connection to the server is required, and as the
+    GSF Server is not done yet, this Frame is still empty.
+    """
+    
     def __init__(self, root_frame):
         ttk.Frame.__init__(self, root_frame)
         self.label = ttk.Label(self, font = ("Calibri", 40),
