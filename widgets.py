002bd83d7735fa19b94d738803ab7fdb10a786ce
RedFantom <redfantom@outlook.com>
2017-02-14 17:04:11 +0100

    Added HoverInfo class by Jakirk Patrick

--- widgets.py
+++ widgets.py
@@ -9,6 +9,7 @@ except ImportError:
 import platform
 import calendar
 import tkFont
+import re
 from PIL import Image, ImageTk
 import os
 
@@ -379,3 +380,48 @@ class ToggledFrame(ttk.Frame):
         else:
             self.sub_frame.forget()
             self.toggle_button.configure(image = self.closed)
+
+
+class HoverInfo(tk.Menu):
+    """"
+    A simple class that provides an info box when hovering over a Tkinter widget
+    Author: Jakirk Patrick
+    License: None
+    Source: https://jakirkpatrick.wordpress.com/2012/02/01/making-a-hovering-box-in-tkinter/
+
+    Edited by RedFantom: Removed comparisons to None
+    """
+
+    def __init__(self, parent, text, command=None):
+        self._com = command
+        tk.Menu.__init__(self, parent, tearoff=0)
+        if not isinstance(text, str):
+            raise TypeError('Trying to initialise a Hover Menu with a non string type: ' + text.__class__.__name__)
+        toktext = re.split('\n', text)
+        for t in toktext:
+            self.add_command(label=t)
+        self._displayed = False
+        self.master.bind("<Enter>", self.Display)
+        self.master.bind("<Leave>", self.Remove)
+
+    def __del__(self):
+        self.master.unbind("<Enter>")
+        self.master.unbind("<Leave>")
+
+    def Display(self, event):
+        if not self._displayed:
+            self._displayed = True
+            self.post(event.x_root, event.y_root)
+        if not self._com:
+            self.master.unbind_all("<Return>")
+            self.master.bind_all("<Return>", self.Click)
+
+    def Remove(self, event):
+        if self._displayed:
+            self._displayed = False
+            self.unpost()
+        if not self._com:
+            self.unbind_all("<Return>")
+
+    def Click(self, event):
+        self._com()
