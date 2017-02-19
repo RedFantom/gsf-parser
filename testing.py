28f4157e7266785cdae0e6cff2a965cfbcbbbcee
RedFantom <redfantom@outlook.com>
2017-02-19 21:06:38 +0100

    Added header to testing.py

--- testing.py
+++ testing.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+
+# Written by RedFantom, Wing Commander of Thranta Squadron,
+# Daethyra, Squadron Leader of Thranta Squadron and Sprigellania, Ace of Thranta Squadron
+# Thranta Squadron GSF CombatLog Parser, Copyright (C) 2016 by RedFantom, Daethyra and Sprigellania
+# All additions are under the copyright of their respective authors
+# For license see LICENSE
 import unittest
 import parse
 from datetime import datetime
