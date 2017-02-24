6cd0bde6ac6e4d44fcc9cb962e0b734fecc7e497
RedFantom <RedFantom@users.noreply.github.com>
2017-02-24 16:08:21 +0100

    Merge platform into development for #54 (#63)
    * Added debug message for platform detection
    * Update platforms for #54
    * Changed lower to upper in post2008server
    * Updated with lower() for #54

--- realtimeframe.py
+++ realtimeframe.py
@@ -111,7 +111,9 @@ class realtime_frame(ttk.Frame):
             self.parsing_bar.start(3)
             self.start_parsing_button.configure(text="Stop real-time parsing")
         elif self.parsing:
-            if platform.release() != "10" and platform.release() != "8" and platform.release() != "8.1":
+            print "[DEBUG] Detected Windows version: ", platform.release()
+            if (platform.release() != "10" and platform.release() != "8" and platform.release() != "8.1"
+                and platform.release().lower() != "post2008Server".lower()):
                 self.main_window.file_select_frame.add_files_cb()
             self.parsing = False
             variables.FLAG = False
