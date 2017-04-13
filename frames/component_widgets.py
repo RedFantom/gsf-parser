85ffa629c00f332c8fdd6ee1133546773c80a517
RedFantom <redfantom@outlook.com>
2017-04-13 18:30:54 +0200

    Implement MiddleComponentWidget layout
    Also fixed the path generation for the new icons folder and some other
    minor tiny things.

--- frames/component_widgets.py
+++ frames/component_widgets.py
@@ -10,7 +10,6 @@ import ttk
 from os import path
 from PIL import Image as img
 from PIL.ImageTk import PhotoImage as photo
-from variables import settings_obj as settings
 from widgets import HoverInfo
 
 
@@ -18,14 +17,7 @@ class ComponentWidget(ttk.Frame):
     def __init__(self, parent, data_dictionary):
         ttk.Frame.__init__(self, parent)
         self.data_dictionary = data_dictionary
-        if settings.faction == "imperial":
-            self.icons_path = path.abspath(path.join(path.dirname(path.realpath(__file__)), "..", "assets", "icons",
-                                                     "imperial"))
-        elif settings.faction == "republic":
-            self.icons_path = path.abspath(path.join(path.dirname(path.realpath(__file__)), "..", "assets", "icons",
-                                                     "republic"))
-        else:
-            raise ValueError("Unexpected value for faction found.")
+        self.icons_path = path.abspath(path.join(path.dirname(path.realpath(__file__)), "..", "assets", "icons"))
 
     def __getitem__(self, key):
         return self.data_dictionary[key]
@@ -33,15 +25,74 @@ class ComponentWidget(ttk.Frame):
     def __setitem__(self, key, value):
         self.data_dictionary[key] = value
 
+    def set_level(self, level):
+        raise NotImplementedError
+
+    def grid_widgets(self):
+        raise NotImplementedError
+
 
 class MajorComponentWidget(ComponentWidget):
     def __init__(self, parent, data_dictionary, ship):
         ComponentWidget.__init__(self, parent, data_dictionary)
 
 
-class MiddleComponentFrame(ComponentWidget):
+class MiddleComponentWidget(ComponentWidget):
     def __init__(self, parent, data_dictionary, ship):
         ComponentWidget.__init__(self, parent, data_dictionary)
+        ComponentWidget.__init__(self, parent, data_dictionary)
+        self.description = data_dictionary["Description"]
+        self.description_label = ttk.Label(self, text=self.description, justify=tk.LEFT, wraplength=200)
+        self.icon = data_dictionary["Icon"] + ".jpg"
+        self.icon_image = img.open(path.join(self.icons_path, self.icon))
+        self.icon_photo = photo(self.icon_image)
+        self.icon_label = ttk.Label(self, image=self.icon_photo)
+        self.upgrade_buttons = []
+        self.hover_infos = []
+        self.photos = []
+        for i in range(3):
+            if i == 2:
+                self.photos.append(photo(img.open(path.join(self.icons_path,
+                                                            data_dictionary["TalentTree"][i][0]["Icon"] + ".jpg"))))
+                self.photos.append(photo(img.open(path.join(self.icons_path,
+                                                            data_dictionary["TalentTree"][i][1]["Icon"] + ".jpg"))))
+                self.upgrade_buttons.append([ttk.Button(self, image=self.photos[i],
+                                                        command=lambda: press_button(self.upgrade_buttons[i][0],
+                                                                                     self.set_level, i)),
+                                             ttk.Button(self, image=self.photos[i + 1],
+                                                        command=lambda: press_button(self.upgrade_buttons[i][1],
+                                                                                     self.set_level, i + 1))])
+                self.hover_infos.append([HoverInfo(self.upgrade_buttons[i][0],
+                                                   text=str(data_dictionary["TalentTree"][i][0]["Name"]) + "\n\n" +
+                                                        str(data_dictionary["TalentTree"][i][0]["Description"]),
+                                                   width=50),
+                                         HoverInfo(self.upgrade_buttons[i][1],
+                                                   text=str(data_dictionary["TalentTree"][i][1]["Name"]) + "\n\n" +
+                                                        str(data_dictionary["TalentTree"][i][1]["Description"]),
+                                                   width=50)])
+            else:
+                self.photos.append(photo(img.open(path.join(self.icons_path,
+                                                            data_dictionary["TalentTree"][i][0]["Icon"] + ".jpg"))))
+                self.upgrade_buttons.append(ttk.Button(self, image=self.photos[i],
+                                                       command=lambda: press_button(self.upgrade_buttons[i],
+                                                                                    self.set_level, i)))
+                self.hover_infos.append(HoverInfo(self.upgrade_buttons[i],
+                                                  text=str(data_dictionary["TalentTree"][i][0]["Name"]) + "\n\n" +
+                                                       str(data_dictionary["TalentTree"][i][0]["Description"])))
+
+    def grid_widgets(self):
+        self.description_label.grid(row=0, column=0, columnspan=3)
+        set_row = 1
+        for widget in self.upgrade_buttons:
+            if isinstance(widget, list):
+                widget[0].grid(row=set_row, column=0)
+                widget[1].grid(row=set_row, column=2)
+            else:
+                widget.grid(row=set_row, column=1)
+            set_row += 1
+
+    def set_level(self, level):
+        pass
 
 
 class MinorComponentWidget(ComponentWidget):
@@ -53,6 +104,7 @@ class MinorComponentWidget(ComponentWidget):
 
     Levels: 0, 1, 2, 3
     """
+
     def __init__(self, parent, data_dictionary, ship):
         ComponentWidget.__init__(self, parent, data_dictionary)
         self.description = data_dictionary["Description"]
@@ -66,7 +118,7 @@ class MinorComponentWidget(ComponentWidget):
         for i in range(3):
             self.upgrade_buttons.append(ttk.Button(self, image=self.icon_photo,
                                                    command=lambda: press_button(self.upgrade_buttons[i],
-                                                                                self.enable_level, i+1)))
+                                                                                self.set_level, i + 1)))
             self.hover_infos.append(HoverInfo(self.upgrade_buttons[i], str(data_dictionary["TalentTree"][i][0]["Name"])
                                               + "\n\n" + str(data_dictionary["TalentTree"][i][0]["Description"]),
                                               width=50))
@@ -80,7 +132,7 @@ class MinorComponentWidget(ComponentWidget):
             widget.grid(row=set_row, column=0, pady=5)
             set_row += 1
 
-    def enable_level(self, level):
+    def set_level(self, level):
         pass
 
 
