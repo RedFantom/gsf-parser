dc283ac9630d674da7565df8f1f88e46db9b4e62
RedFantom <redfantom@outlook.com>
2017-04-17 17:02:56 +0200

    Updated formatting, small fixes, new dictionary
    Added the all_ships dictionary to include the Infiltrator class ships,
    the formatting of all lists and dictionaries has been updated for more
    clarity in this quite large file and there were some items with no
    commas between them in the ship abilities lists, which could cause
    problems in file parsing.

--- parsing/abilities.py
+++ parsing/abilities.py
@@ -103,103 +103,155 @@ These lists were made with the help of Yellowbird
 
 Lists of the components supported by each ship
 """
-legionAbilities = ["Heavy Laser Cannon", "Laser Cannon", "Light Laser Cannon",
-                   "Proton Torpedo", "Concussion Missile", "Seeker Mine",
-                   "Shield Power Converter", "Interdiction Drive",
-                   "Railgun", "Railgun Sentry Drone", "Interdiction Sentry Drone", "Missile Sentry Drone",
-                   # Railgun = Railgun Sentry Drone
-                   "Shield Projector", "Repair Drone", "Overcharged Shield"]
-razorwireAbilities = ["Heavy Laser Cannon", "Laser Cannon", "Light Laser Cannon",
-                      "Seismic Mine", "Proton Torpedo", "Seeker Mine",
-                      "Shield Power Converter", "Interdiction Drive", "Hyperspace Beacon",
-                      "Interdiction Mine", "Concussion Mine", "Ion Mine",
-                      "Charged Plating", "Overcharged Shield", "Shield Projector"]
-decimusAbilities = ["Heavy Laser Cannon", "Laser Cannon", "Light Laser Cannon", "Quad Laser Cannon",
-                    "Cluster Missiles", "Concussion Missile", "Proton Torpedo",
-                    "Shield Power Converter", "Power Dive", "Interdiction Drive",
-                    "Ion Mine", "Concussion Mine", "Interdiction Sentry Drone",
-                    "Overcharged Shield", "Directional Shield", "Charged Plating"]
-jurgoranAbilities = ["Burst Laser Cannon", "Light Laser Cannon", "Laser Cannon",
-                     "Cluster Missiles", "Slug Railgun", "Interdiction Missile", "EMP Missile"
-                                                                                 "Koiogran Turn", "Retro Thrusters",
-                     "Power Dive", "Interdiction Drive",
-                     "Directional Shield", "Feedback Shield", "Distortion Field", "Fortress Shield"]
-dustmakerAbilities = ["Laser Cannon", "Heavy Laser Cannon",
-                      "Proton Torpedo", "Thermite Torpedo", "Plasma Railgun", "Slug Railgun",
-                      "Weapon Power Converter", "Rotational Thrusters", "Interdiction Drive", "Barrel Roll",
-                      "Fortress Shield", "Directional Shield", "Feedback Shield"]
-manglerAbilities = ["Light Laser Cannon", "Burst Laser Cannon",
-                    "Plasma Railgun", "Slug Railgun", "Ion Railgun",
-                    "Rotational Thrusters", "Barrel Roll", "Interdiction Drive", "Weapon Power Converter",
-                    "Feedback Shield", "Fortress Shield", "Distortion Field"]
-bloodmarkAbilities = ["Light Laser Cannon", "Laser Cannon", "Rapid-fire Laser Cannon",
-                      "Ion Missile", "EMP Missile", "Thermite Torpedo",
-                      "Snap Turn", "Power Dive", "Interdiction Drive", "Koiogran Turn"
-                                                                       "Combat Command", "Tensor Field",
-                      "Sensor Beacon", "Targeting Telemetry",
-                      "Shield Projector", "Repair Drone", "Distortion Field"]
-blackboltAbilities = ["Rapid-fire Laser Cannon", "Light Laser Cannon", "Laser Cannon",
-                      "Rocket Pod", "Thermite Torpedo", "Sabotage Probe",
-                      "Power Dive", "Snap Turn", "Barrel Roll", "Koiogran Turn",
-                      "Targeting Telemetry", "EMP Field", "Booster Recharge", "Sensor Beacon",
-                      "Distortion Field", "Quick-Charge Shield", "Engine Power Converter"]
-stingAbilities = ["Burst Laser Cannon", "Light Laser Cannon", "Quad Laser Cannon", "Rapid-fire Laser Cannon",
-                  "Rocket Pod", "Cluster Missiles", "Sabotage Probe"
-                                                    "Koiogran Turn", "Retro Thrusters", "Power Dive", "Barrel Roll",
-                  "Targeting Telemetry", "Blaster Overcharge", "Booster Recharge",
-                  "Distortion Field", "Quick-Charge Shield", "Directional Shield"]
-imperiumAbilities = ["Quad Laser Cannon", "Rapid-fire Laser Cannon", "Light Laser Cannon",
-                     "Thermite Torpedo", "EMP Missile", "Proton Torpedo", "Ion Missile",
-                     "Koiogran Turn", "Shield Power Converter", "Power Dive", "Interdiction Drive",
-                     "Combat Command", "Remote Slicing", "Repair Probes",
-                     "Charged Plating", "Directional Shield", "Shield Projector"]
-rycerAbilities = ["Quad Laser Cannon", "Ion Cannon", "Rapid-fire Laser Cannon", "Heavy Laser Cannon", "Laser Cannon",
-                  "Concussion Missile", "Cluster Missiles", "Proton Torpedo",
-                  "Weapon Power Converter", "Retro Thrusters", "Barrel Roll", "Koiogran Turn",
-                  "Charged Plating", "Quick-Charge Shield", "Directional Shield"]
-quellAbilities = ["Heavy Laser Cannon", "Quad Laser Cannon", "Light Laser Cannon",
-                  "Cluster Missiles", "Ion Missile", "Proton Torpedo", "Concussion Missile", "EMP Missile",
-                  "Weapon Power Converter", "Shield Power Converter", "Koiogran Turn", "Barrel Roll",
-                  "Quick-Charge Shield", "Directional Shield", "Charged Plating"]
-excluded_abilities = ["Wingman", "Hydro Spanner", "In Your Sights", "Slicer's Loop",
-                      "Servo Jammer", "Lockdown", "Concentrated Fire", "Lingering Effect",
-                      "Bypass", "Running Interference", "Suppression", "Nullify", "Hull Cutter",
-                      "Selfdamage", "Secondary Weapon Swap", "Primary Weapon Swap", "Sabotage Probe",
-                      "Plasma Burn", "Plasma Warheads", "Space Exhaustion", "Invulnerable"]
+legionAbilities = [
+    "Heavy Laser Cannon", "Laser Cannon", "Light Laser Cannon",
+    "Proton Torpedo", "Concussion Missile", "Seeker Mine",
+    "Shield Power Converter", "Interdiction Drive",
+    "Railgun", "Railgun Sentry Drone", "Interdiction Sentry Drone", "Missile Sentry Drone",
+    # Railgun = Railgun Sentry Drone
+    "Shield Projector", "Repair Drone", "Overcharged Shield"
+]
+razorwireAbilities = [
+    "Heavy Laser Cannon", "Laser Cannon", "Light Laser Cannon",
+    "Seismic Mine", "Proton Torpedo", "Seeker Mine",
+    "Shield Power Converter", "Interdiction Drive", "Hyperspace Beacon",
+    "Interdiction Mine", "Concussion Mine", "Ion Mine",
+    "Charged Plating", "Overcharged Shield", "Shield Projector"
+]
+decimusAbilities = [
+    "Heavy Laser Cannon", "Laser Cannon", "Light Laser Cannon", "Quad Laser Cannon",
+    "Cluster Missiles", "Concussion Missile", "Proton Torpedo",
+    "Shield Power Converter", "Power Dive", "Interdiction Drive",
+    "Ion Mine", "Concussion Mine", "Interdiction Sentry Drone",
+    "Overcharged Shield", "Directional Shield", "Charged Plating"
+]
+jurgoranAbilities = [
+    "Burst Laser Cannon", "Light Laser Cannon", "Laser Cannon",
+    "Cluster Missiles", "Slug Railgun", "Interdiction Missile", "EMP Missile",
+    "Koiogran Turn", "Retro Thrusters",
+    "Power Dive", "Interdiction Drive",
+    "Directional Shield", "Feedback Shield", "Distortion Field", "Fortress Shield"
+]
+dustmakerAbilities = [
+    "Laser Cannon", "Heavy Laser Cannon",
+    "Proton Torpedo", "Thermite Torpedo", "Plasma Railgun", "Slug Railgun",
+    "Weapon Power Converter", "Rotational Thrusters", "Interdiction Drive", "Barrel Roll",
+    "Fortress Shield", "Directional Shield", "Feedback Shield"
+]
+manglerAbilities = [
+    "Light Laser Cannon", "Burst Laser Cannon",
+    "Plasma Railgun", "Slug Railgun", "Ion Railgun",
+    "Rotational Thrusters", "Barrel Roll", "Interdiction Drive", "Weapon Power Converter",
+    "Feedback Shield", "Fortress Shield", "Distortion Field"
+]
+bloodmarkAbilities = [
+    "Light Laser Cannon", "Laser Cannon", "Rapid-fire Laser Cannon",
+    "Ion Missile", "EMP Missile", "Thermite Torpedo",
+    "Snap Turn", "Power Dive", "Interdiction Drive", "Koiogran Turn",
+    "Combat Command", "Tensor Field",
+    "Sensor Beacon", "Targeting Telemetry",
+    "Shield Projector", "Repair Drone", "Distortion Field"
+]
+blackboltAbilities = [
+    "Rapid-fire Laser Cannon", "Light Laser Cannon", "Laser Cannon",
+    "Rocket Pod", "Thermite Torpedo", "Sabotage Probe",
+    "Power Dive", "Snap Turn", "Barrel Roll", "Koiogran Turn",
+    "Targeting Telemetry", "EMP Field", "Booster Recharge", "Sensor Beacon",
+    "Distortion Field", "Quick-Charge Shield", "Engine Power Converter"
+]
+stingAbilities = [
+    "Burst Laser Cannon", "Light Laser Cannon", "Quad Laser Cannon", "Rapid-fire Laser Cannon",
+    "Rocket Pod", "Cluster Missiles", "Sabotage Probe",
+    "Koiogran Turn", "Retro Thrusters", "Power Dive", "Barrel Roll",
+    "Targeting Telemetry", "Blaster Overcharge", "Booster Recharge",
+    "Distortion Field", "Quick-Charge Shield", "Directional Shield"
+]
+imperiumAbilities = [
+    "Quad Laser Cannon", "Rapid-fire Laser Cannon", "Light Laser Cannon",
+    "Thermite Torpedo", "EMP Missile", "Proton Torpedo", "Ion Missile",
+    "Koiogran Turn", "Shield Power Converter", "Power Dive", "Interdiction Drive",
+    "Combat Command", "Remote Slicing", "Repair Probes",
+    "Charged Plating", "Directional Shield", "Shield Projector"
+]
+rycerAbilities = [
+    "Quad Laser Cannon", "Ion Cannon", "Rapid-fire Laser Cannon", "Heavy Laser Cannon", "Laser Cannon",
+    "Concussion Missile", "Cluster Missiles", "Proton Torpedo",
+    "Weapon Power Converter", "Retro Thrusters", "Barrel Roll", "Koiogran Turn",
+    "Charged Plating", "Quick-Charge Shield", "Directional Shield"
+]
+quellAbilities = [
+    "Heavy Laser Cannon", "Quad Laser Cannon", "Light Laser Cannon",
+    "Cluster Missiles", "Ion Missile", "Proton Torpedo", "Concussion Missile", "EMP Missile",
+    "Weapon Power Converter", "Shield Power Converter", "Koiogran Turn", "Barrel Roll",
+    "Quick-Charge Shield", "Directional Shield", "Charged Plating"
+]
+excluded_abilities = [
+    "Wingman", "Hydro Spanner", "In Your Sights", "Slicer's Loop",
+    "Servo Jammer", "Lockdown", "Concentrated Fire", "Lingering Effect",
+    "Bypass", "Running Interference", "Suppression", "Nullify", "Hull Cutter",
+    "Selfdamage", "Secondary Weapon Swap", "Primary Weapon Swap", "Sabotage Probe",
+    "Plasma Burn", "Plasma Warheads", "Space Exhaustion", "Invulnerable"
+]
 
 # All ships with their Imperial Faction names available in GSF
-ships = ["Legion", "Decimus", "Razorwire", "Jurgoran", "Dustmaker", "Mangler", "Bloodmark", "Blackbolt", "Sting",
-         "Imperium", "Rycer", "Quell"]
+ships = [
+    "Legion", "Decimus", "Razorwire", "Jurgoran", "Dustmaker", "Mangler", "Bloodmark", "Blackbolt", "Sting",
+    "Imperium", "Rycer", "Quell"
+]
 
 # A dictionary for converting the names of the ships to the Republic Faction
 # names, for future feature of allowing both faction names
-rep_ships = {"Legion": "Warcarrier",
-             "Razorwire": "Rampart Mark Four",
-             "Decimus": "Sledgehammer",
-             "Mangler": "Quarrel",
-             "Jurgoran": "Condor",
-             "Dustmaker": "Comet Breaker",
-             "Rycer": "Star Guard",
-             "Imperium": "Clarion",
-             "Quell": "Pike",
-             "Sting": "Flashfire",
-             "Bloodmark": "Spearpoint",
-             "Blackbolt": "Novadive"}
+rep_ships = {
+    "Legion": "Warcarrier",
+    "Razorwire": "Rampart Mark Four",
+    "Decimus": "Sledgehammer",
+    "Mangler": "Quarrel",
+    "Jurgoran": "Condor",
+    "Dustmaker": "Comet Breaker",
+    "Rycer": "Star Guard",
+    "Imperium": "Clarion",
+    "Quell": "Pike",
+    "Sting": "Flashfire",
+    "Bloodmark": "Spearpoint",
+    "Blackbolt": "Novadive"
+}
 
 # The ships with their Imperial Faction names to print on the screen, with \t padding
 # to make the printing look the same for every ship.
-ships_strings = ["Legion\t", "Decimus\t", "Razorwire\t", "Jurgoran\t", "Dustmaker", "Mangler\t", "Bloodmark",
-                 "Blackbolt\t", "Sting\t", "Imperium\t", "Rycer\t", "Quell\t"]
+ships_strings = [
+    "Legion\t", "Decimus\t", "Razorwire\t", "Jurgoran\t", "Dustmaker", "Mangler\t", "Bloodmark",
+    "Blackbolt\t", "Sting\t", "Imperium\t", "Rycer\t", "Quell\t"
+]
 
-rep_strings = {"Legion\t": "Warcarrier",
-             "Razorwire\t": "Rampart Mark Four",
-             "Decimus\t": "Sledgehammer",
-             "Mangler\t": "Quarrel\t",
-             "Jurgoran\t": "Condor\t",
-             "Dustmaker": "Comet Breaker",
-             "Rycer\t": "Star Guard",
-             "Imperium\t": "Clarion\t",
-             "Quell\t": "Pike\t",
-             "Sting\t": "Flashfire\t",
-             "Bloodmark": "Spearpoint",
-             "Blackbolt\t": "Novadive\t"}
+rep_strings = {
+    "Legion\t": "Warcarrier",
+    "Razorwire\t": "Rampart Mark Four",
+    "Decimus\t": "Sledgehammer",
+    "Mangler\t": "Quarrel\t",
+    "Jurgoran\t": "Condor\t",
+    "Dustmaker": "Comet Breaker",
+    "Rycer\t": "Star Guard",
+    "Imperium\t": "Clarion\t",
+    "Quell\t": "Pike\t",
+    "Sting\t": "Flashfire\t",
+    "Bloodmark": "Spearpoint",
+    "Blackbolt\t": "Novadive\t"
+}
+
+all_ships = {
+    "Legion": "Warcarrier",
+    "Razorwire": "Rampart Mark Four",
+    "Decimus": "Sledgehammer",
+    "Mangler": "Quarrel",
+    "Jurgoran": "Condor",
+    "Dustmaker": "Comet Breaker",
+    "Rycer": "Star Guard",
+    "Imperium": "Clarion",
+    "Quell": "Pike",
+    "Sting": "Flashfire",
+    "Bloodmark": "Spearpoint",
+    "Blackbolt": "Novadive",
+    "Frostburn": "Whisper",
+    "Sable Claw": "Mirage",
+    "Tormentor": "Banshee"
+}
