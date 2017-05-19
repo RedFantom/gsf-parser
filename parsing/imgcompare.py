5749f6b5c5654318c3f378531c6ce23211c9ec31
RedFantom <redfantom@outlook.com>
2017-05-19 21:58:15 +0200

    Added simple image comparison function

---
+++ parsing/imgcompare.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# Written by RedFantom, Wing Commander of Thranta Squadron,
+# Daethyra, Squadron Leader of Thranta Squadron and Sprigellania, Ace of Thranta Squadron
+# Thranta Squadron GSF CombatLog Parser, Copyright (C) 2016 by RedFantom, Daethyra and Sprigellania
+# All additions are under the copyright of their respective authors
+# For license see LICENSE
+from PIL import Image
+
+
+def get_similarity(image_one, image_two):
+    """
+    Compares two images and returns the similarity ratio. Based upon code of a StackOverflow question that I can't seem
+    to find to put the link here. Anyway, RedFantom has rewritten this code to work with Python 3 and to comply with
+    PEP-8.
+    :param image_one: Image object or path
+    :param image_two: Image object or path
+    :return: ratio
+    """
+    if not isinstance(image_one, Image):
+        image_one = Image.open(image_one)
+    if not isinstance(image_two, Image):
+        image_two = Image.open(image_two)
+    if image_one.size != image_two.size:
+        raise ValueError("These images are not the same size.")
+    pairs = zip(image_one.getdata(), image_two.getdata())
+    diff = sum(abs(c1 - c2) for p1, p2 in pairs for c1, c2 in zip(p1, p2))
+    ncomponents = image_one.size[0] * image_one.size[1] * 3
+    return (diff / 255.0 * 100) / ncomponents
