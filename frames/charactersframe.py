6f98e9ac8f617ce3b36f16635691d797af758244
RedFantom <redfantom@outlook.com>
2017-06-03 19:27:11 +0200

    Add ship line-up Checkbuttons

--- frames/charactersframe.py
+++ frames/charactersframe.py
@@ -16,6 +16,8 @@ from tools.utilities import get_assets_directory
 from parsing.guiparsing import GSFInterface, get_gui_profiles
 from toplevels.addcharacter import AddCharacter
 import variables
+from parsing import abilities
+from collections import OrderedDict
 
 
 class CharactersFrame(ttk.Frame):
@@ -67,12 +69,15 @@ class CharactersFrame(ttk.Frame):
         self.legacy_name_label = ttk.Label(self.options_frame, text="Legacy name")
         self.legacy_name_entry = ttk.Entry(self.options_frame, width=35)
         self.faction = tk.StringVar()
+        self.faction.set("imperial")
         self.faction_label = ttk.Label(self.options_frame, text="Faction")
         self.faction_republic_radiobutton = ttk.Radiobutton(self.options_frame, text="Republic",
                                                             image=self.republic_logo, compound=tk.LEFT,
-                                                            variable=self.faction, value="republic")
+                                                            variable=self.faction, value="republic",
+                                                            command=lambda: self.set_character_faction("republic"))
         self.faction_imperial_radiobutton = ttk.Radiobutton(self.options_frame, text="Empire", image=self.imperial_logo,
-                                                            compound=tk.LEFT, variable=self.faction, value="imperial")
+                                                            compound=tk.LEFT, variable=self.faction, value="imperial",
+                                                            command=lambda: self.set_character_faction("imperial"))
         self.gui_profile = tk.StringVar()
         self.gui_profile_label = ttk.Label(self.options_frame, text="GUI Profile")
         self.gui_profile_dropdown = ttk.OptionMenu(self.options_frame, self.gui_profile, *tuple(get_gui_profiles()))
@@ -80,7 +85,22 @@ class CharactersFrame(ttk.Frame):
         self.lineup_label = ttk.Label(self.options_frame, text="Ships line-up")
         self.lineup_frame = ttk.Frame(self.options_frame)
 
+        self.imp_ship_widgets = OrderedDict()
+        self.imp_ship_variables = OrderedDict()
+        self.rep_ship_widgets = OrderedDict()
+        self.rep_ship_variables = OrderedDict()
+
+        for ship_name in abilities.all_ships.keys():
+            self.imp_ship_variables[ship_name] = tk.IntVar()
+            self.imp_ship_widgets[ship_name] = ttk.Checkbutton(self.lineup_frame, text=ship_name,
+                                                               variable=self.imp_ship_variables[ship_name])
+        for ship_name in abilities.all_ships.values():
+            self.rep_ship_variables[ship_name] = tk.IntVar()
+            self.rep_ship_widgets[ship_name] = ttk.Checkbutton(self.lineup_frame, text=ship_name,
+                                                               variable=self.rep_ship_variables[ship_name])
+
     def grid_widgets(self):
+        self.widgets_grid_forget()
         self.characters_list.grid(column=0, row=0, sticky="nswe", padx=5, pady=5)
         self.characters_scroll.grid(column=1, row=0, sticky="ns", pady=5)
         self.new_character_button.grid(column=0, row=1, sticky="nswe", pady=5, padx=5)
@@ -96,7 +116,31 @@ class CharactersFrame(ttk.Frame):
         self.gui_profile_dropdown.grid(column=0, row=5, sticky="nswe", padx=5, pady=5)
         self.gui_profile_detect_button.grid(column=1, row=5, sticky="nswe", padx=5, pady=5)
         self.lineup_label.grid(column=0, row=6, sticky="nsw", padx=5, pady=5)
-        self.lineup_frame.grid(column=0, row=7, rowspan=2, sticky="nswe", padx=5, pady=5)
+        self.lineup_frame.grid(column=0, row=7, rowspan=2, columnspan=3, sticky="nswe", padx=5, pady=5)
+        set_row = 0
+        set_column = 0
+        if self.faction.get() == "imperial":
+            for item in self.imp_ship_widgets.values():
+                item.grid(row=set_row, column=set_column, padx=5, pady=5, sticky="w")
+                set_row += 1
+                if set_row == 5:
+                    set_column += 1
+                    set_row = 0
+        elif self.faction.get() == "republic":
+            for item in self.rep_ship_widgets.values():
+                item.grid(row=set_row, column=set_column, pady=5, sticky="w")
+                set_row += 1
+                if set_row == 5:
+                    set_column += 1
+                    set_row = 0
+        else:
+            raise ValueError("Invalid value for faction found: {0}".format(self.faction.get()))
+
+    def widgets_grid_forget(self):
+        for item in self.imp_ship_widgets.values():
+            item.grid_forget()
+        for item in self.rep_ship_widgets.values():
+            item.grid_forget()
 
     def new_database(self):
         characters = {server: None for server in self.servers}
@@ -111,4 +155,10 @@ class CharactersFrame(ttk.Frame):
         AddCharacter(variables.main_window, tuple(self.servers.values()), self.insert_character)
 
     def insert_character(self, name, legacy, server, faction):
+        pass
+
+    def set_character_faction(self, faction):
+        print("Setting faction {0}".format(faction))
+        print("self.faction == {0}".format(self.faction.get()))
+        self.grid_widgets()
         pass
