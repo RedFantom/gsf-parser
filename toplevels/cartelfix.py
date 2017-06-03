ea93b72c105805a046a7149c6164b1d5e17a1168
RedFantom <redfantom@outlook.com>
2017-06-03 13:53:03 +0200

    Fix CartelFix

--- toplevels/cartelfix.py
+++ toplevels/cartelfix.py
@@ -5,25 +5,36 @@
 # For license see LICENSE
 import tkinter as tk
 from pynput import keyboard
-import cv2
 from PIL import Image, ImageTk
-from parsing.vision import pillow_to_numpy
-from tools.utilities import get_pillow_screen
-import numpy
+import variables
 
 
 class CartelFix(tk.Toplevel):
-    def __init__(self, master, first, second):
+    def __init__(self, master, first, second, coordinates, scale):
         tk.Toplevel.__init__(self, master)
         self.label = tk.Label(self)
         self.railgun = 1
-        self.first_pil = Image.open(first)
-        self.first = ImageTk.PhotoImage(self.first_pil)
-        self.second = ImageTk.PhotoImage(Image.open(second))
+        size = (int(round(45.0 * scale, 0)), int(round(45.0 * scale, 0)))
+        self.first = ImageTk.PhotoImage(Image.open(first).resize(size, Image.ANTIALIAS))
+        self.second = ImageTk.PhotoImage(Image.open(second).resize(size, Image.ANTIALIAS))
         self.label.config(image=self.first)
+        self.label.grid()
         self.listener = keyboard.Listener(on_press=self.switch)
+        self.coordinates = coordinates
+        self.set_geometry()
 
-    def switch(self, *args):
+    def switch(self, key):
+        # This annoying code path is necessary because pynput does not provide full documentation
+        # key.char is only provided by the pynput library if the key is an alphanumeric key
+        # It is unknown whether key.char is always a str, or can also be int when a numeric key is pressed
+        # The return value of this function is always True to keep the pynput thread running
+        try:
+            if int(key.char) != 1:
+                return True
+        except AttributeError:
+            return True
+        except TypeError:
+            return True
         if self.railgun == 1:
             self.label.config(image=self.second)
             self.railgun = 2
@@ -32,15 +43,18 @@ class CartelFix(tk.Toplevel):
             self.railgun = 1
         else:
             raise ValueError("Not a valid railgun number: {0}".format(self.railgun))
+        return True
 
     def set_geometry(self):
-        screen = pillow_to_numpy(get_pillow_screen())
-        template = pillow_to_numpy(self.first_pil)
-        results = cv2.matchTemplate(screen, template, cv2.TM_CCOEFF_NORMED)
-        y, x = numpy.unravel_index(results.argmax(), results.shape)
+        self.configure(background="white")
+        self.wm_attributes("-transparentcolor", "white")
         self.overrideredirect(True)
         self.attributes("-topmost", True)
-        self.wm_geometry("+{0}+{1}".format(x, y))
+        self.wm_geometry("+{0}+{1}".format(self.coordinates[0], self.coordinates[1]))
 
     def start_listener(self):
         self.listener.start()
+
+    def destroy(self):
+        variables.main_window.realtime_frame.cartelfix = None
+        super(tk.Toplevel, self)
