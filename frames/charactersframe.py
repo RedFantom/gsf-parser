7ef9fd36e38f4c48a3133190b4014f262e354090
RedFantom <redfantom@outlook.com>
2017-06-04 22:29:52 +0200

    Implement most of CharactersFrame

--- frames/charactersframe.py
+++ frames/charactersframe.py
@@ -13,14 +13,19 @@ from tools import utilities
 from widgets import VerticalScrollFrame
 from PIL import Image, ImageTk
 from tools.utilities import get_assets_directory
-from parsing.guiparsing import GSFInterface, get_gui_profiles
+from parsing.guiparsing import get_gui_profiles, get_player_guiname
 from toplevels.addcharacter import AddCharacter
 import variables
 from parsing import abilities
 from collections import OrderedDict
+from tkinter import messagebox as mb
 
 
 class CharactersFrame(ttk.Frame):
+    """
+    A frame in which the user can enter his characters in simple, organized manner, so the GSF Parser can make good use
+    of this data in
+    """
     def __init__(self, parent):
         ttk.Frame.__init__(self, parent)
         self.directory = utilities.get_temp_directory()
@@ -59,7 +64,9 @@ class CharactersFrame(ttk.Frame):
         try:
             with open(os.path.join(self.directory, "characters.db"), "rb") as f:
                 self.characters = pickle.load(f)
-        except OSError or EOFError:
+        except OSError:
+            self.new_database()
+        except EOFError:
             self.new_database()
         self.characters_list = ttk.Treeview(self)
         self.characters_scroll = ttk.Scrollbar(self, orient=tk.VERTICAL, command=self.characters_list.yview)
@@ -89,18 +96,19 @@ class CharactersFrame(ttk.Frame):
         self.legacy_name_label = ttk.Label(self.options_frame, text="Legacy name")
         self.legacy_name_entry = ttk.Entry(self.options_frame, width=35)
         self.faction = tk.StringVar()
-        self.faction.set("imperial")
+        self.faction.set("Imperial")
         self.faction_label = ttk.Label(self.options_frame, text="Faction")
         self.faction_republic_radiobutton = ttk.Radiobutton(self.options_frame, text="Republic",
                                                             image=self.republic_logo, compound=tk.LEFT,
-                                                            variable=self.faction, value="republic",
-                                                            command=lambda: self.set_character_faction("republic"))
+                                                            variable=self.faction, value="Republic",
+                                                            command=lambda: self.set_character_faction("Republic"))
         self.faction_imperial_radiobutton = ttk.Radiobutton(self.options_frame, text="Empire", image=self.imperial_logo,
-                                                            compound=tk.LEFT, variable=self.faction, value="imperial",
-                                                            command=lambda: self.set_character_faction("imperial"))
+                                                            compound=tk.LEFT, variable=self.faction, value="Imperial",
+                                                            command=lambda: self.set_character_faction("Imperial"))
         self.gui_profile = tk.StringVar()
         self.gui_profile_label = ttk.Label(self.options_frame, text="GUI Profile")
-        self.gui_profile_dropdown = ttk.OptionMenu(self.options_frame, self.gui_profile, *tuple(get_gui_profiles()))
+        self.gui_profile_dropdown = ttk.OptionMenu(self.options_frame, self.gui_profile,
+                                                   *tuple(("Select", "Default") + tuple(get_gui_profiles())))
         self.gui_profile_detect_button = ttk.Button(self.options_frame, text="Auto detect", command=self.detect_profile)
         self.lineup_label = ttk.Label(self.options_frame, text="Ships line-up")
         self.lineup_frame = ttk.Frame(self.options_frame)
@@ -113,16 +121,28 @@ class CharactersFrame(ttk.Frame):
         for ship_name in abilities.all_ships.keys():
             self.imp_ship_variables[ship_name] = tk.IntVar()
             self.imp_ship_widgets[ship_name] = ttk.Checkbutton(self.lineup_frame, text=ship_name,
-                                                               variable=self.imp_ship_variables[ship_name])
+                                                               variable=self.imp_ship_variables[ship_name],
+                                                               command=self.update_ships)
         for ship_name in abilities.all_ships.values():
             self.rep_ship_variables[ship_name] = tk.IntVar()
             self.rep_ship_widgets[ship_name] = ttk.Checkbutton(self.lineup_frame, text=ship_name,
-                                                               variable=self.rep_ship_variables[ship_name])
+                                                               variable=self.rep_ship_variables[ship_name],
+                                                               command=self.update_ships)
 
-        for identifier, region in self.zones.items():
-            self.characters_list.insert("", tk.END, iid=identifier, text=region)
-            for server in self.regions[identifier]:
-                self.characters_list.insert(identifier, tk.END, iid=server, text=self.servers[server])
+        self.characters_list.bind("<Double-1>", self.set_character)
+        self.character_data = None
+
+        self.update_tree()
+
+    def update_ships(self):
+        if self.character_data["Faction"] == "Imperial":
+            ships = tuple(ship for ship, intvar in self.imp_ship_variables.items() if intvar.get() == 1)
+        elif self.character_data["Faction"] == "Republic":
+            ships = tuple(ship for ship, intvar in self.rep_ship_variables.items() if intvar.get() == 1)
+        else:
+            raise ValueError("Unknown value for faction found: {0}".format(self.character_data["Faction"]))
+        self.character_data["Ships"] = ships
+        self.save_button.invoke()
 
     def grid_widgets(self):
         self.widgets_grid_forget()
@@ -150,14 +170,14 @@ class CharactersFrame(ttk.Frame):
 
         set_row = 0
         set_column = 0
-        if self.faction.get() == "imperial":
+        if self.faction.get() == "Imperial":
             for item in self.imp_ship_widgets.values():
                 item.grid(row=set_row, column=set_column, padx=5, pady=5, sticky="w")
                 set_row += 1
                 if set_row == 5:
                     set_column += 1
                     set_row = 0
-        elif self.faction.get() == "republic":
+        elif self.faction.get() == "Republic":
             for item in self.rep_ship_widgets.values():
                 item.grid(row=set_row, column=set_column, pady=5, sticky="w")
                 set_row += 1
@@ -167,6 +187,17 @@ class CharactersFrame(ttk.Frame):
         else:
             raise ValueError("Invalid value for faction found: {0}".format(self.faction.get()))
 
+    def update_tree(self):
+        self.characters_list.delete(*self.characters_list.get_children())
+
+        for identifier, region in self.zones.items():
+            self.characters_list.insert("", tk.END, iid=identifier, text=region)
+            for server in self.regions[identifier]:
+                self.characters_list.insert(identifier, tk.END, iid=server, text=self.servers[server])
+
+        for character, data in self.characters.items():
+            self.characters_list.insert(data["Server"], tk.END, iid=(data["Server"], data["Name"]), text=data["Name"])
+
     def widgets_grid_forget(self):
         for item in self.imp_ship_widgets.values():
             item.grid_forget()
@@ -174,35 +205,136 @@ class CharactersFrame(ttk.Frame):
             item.grid_forget()
 
     def new_database(self):
+        mb.showinfo("Info", "The GSF Parser is creating a new characters database, discarding all your character data, "
+                            "if you had any, and ship builds. If you did not expect this, please file an issue report "
+                            "in the GitHub repository.")
+        print("Creating new characters database")
         characters = {("TRE", "Example"): {"Server": "TRE",
-                                           "Faction": "imperial",
+                                           "Faction": "Imperial",
                                            "Name": "Example",
                                            "Legacy": "E_Legacy",
-                                           "Ships": ("Blackbolt", "Rycer")}}
+                                           "Ships": ("Blackbolt", "Rycer"),
+                                           "Ship Objects": (None, None),
+                                           "GUI": "Default"}}
         with open(os.path.join(self.directory, "characters.db"), "wb") as f:
             pickle.dump(characters, f)
         self.characters = {}
 
     def detect_profile(self):
-        pass
+        if not self.character_data:
+            mb.showinfo("Info", "Please select a character from the list first.")
+            return
+        try:
+            gui_name = get_player_guiname(self.character_data["Name"])
+        except ValueError:
+            mb.showinfo("Info", "Could not reliably determine the GUI profile used by this character. Please select it "
+                                "manually.")
+            return
+        self.gui_profile.set(gui_name)
+        self.character_data["GUI"] = gui_name
+        self.save_button.invoke()
 
     def new_character(self):
         AddCharacter(variables.main_window, tuple(self.servers.values()), self.insert_character)
 
     def insert_character(self, name, legacy, server, faction):
-        pass
+        if len(server) is not 3:
+            pass
+        if faction == "Imperial":
+            ships = ("Blackbolt", "Rycer")
+        elif faction == "Republic":
+            ships = ("Novadive", "Star Guard")
+        else:
+            raise ValueError("Unkown value for faction found: {0}".format(faction))
+        server = self.reverse_servers[server]
+        self.characters[(server, name)] = {
+            "Server": server,
+            "Faction": faction,
+            "Name": name,
+            "Legacy": legacy,
+            "Ships": ships,
+            "Ship Objects": (None, None),
+            "GUI": "Default"
+        }
+        self.character_data = self.characters[(server, name)]
+        self.clear_character_data()
+        self.set_character(set=False)
+        self.save_button.invoke()
+        self.update_tree()
 
     def set_character_faction(self, faction):
-        print("Setting faction {0}".format(faction))
-        print("self.faction == {0}".format(self.faction.get()))
         self.grid_widgets()
-        pass
+        self.character_data["Faction"] = faction
+        self.save_character_data()
 
     def save_character_data(self):
-        pass
+        self.characters[(self.character_data["Server"], self.character_data["Name"])] = self.character_data
+        with open(os.path.join(self.directory, "characters.db"), "wb") as f:
+            pickle.dump(self.characters, f)
+        print("Saved characters dictionary")
 
     def discard_character_data(self):
-        pass
+        self.clear_character_data()
+        self.set_character()
 
     def delete_character(self):
-        pass
+        del self.characters[(self.character_data["Server"], self.character_data["Name"])]
+        self.characters_list.delete(*((self.character_data["Server"] + " " + self.character_data["Name"]),))
+        self.clear_character_data()
+        self.save_button.invoke()
+        self.update_tree()
+
+    def get_character_data(self):
+        character = self.characters_list.selection()
+        if len(character[0]) < 4:
+            return
+        print(character)
+        server = character[0][:3]
+        name = character[0][4:]
+        if not server:
+            raise ValueError("Server not found: {0}".format(character[0]))
+        self.clear_character_data()
+        try:
+            return self.characters[(server, name)]
+        except KeyError:
+            mb.showerror("Error", "The character {0} was not found in the internal character database. Please report "
+                                  "this error with debug output in the GitHub repository.".format((server, name)))
+            raise ValueError("Character not found {0} in {1}".format((server, name), self.characters))
+
+    def set_character(self, set=True, *args):
+        if not set:
+            character_data = self.character_data
+        else:
+            character_data = self.get_character_data()
+            if not character_data:
+                return
+        self.character_name_entry.insert(tk.END, character_data["Name"])
+        self.legacy_name_entry.insert(tk.END, character_data["Legacy"])
+        self.faction.set(character_data["Faction"])
+        self.gui_profile.set(character_data["GUI"])
+        print(character_data["Ships"])
+        if character_data["Faction"] == "Imperial":
+            for name, intvar in self.imp_ship_variables.items():
+                if name in character_data["Ships"]:
+                    intvar.set(1)
+                else:
+                    intvar.set(0)
+        elif character_data["Faction"] == "Republic":
+            for name, intvar in self.rep_ship_variables.items():
+                if name in character_data["Ships"]:
+                    intvar.set(1)
+                else:
+                    intvar.set(0)
+        else:
+            raise ValueError("Unkown faction value found: {0}".format(character_data["Faction"]))
+        self.character_data = character_data
+
+    def clear_character_data(self):
+        self.character_name_entry.delete(0, tk.END)
+        self.legacy_name_entry.delete(0, tk.END)
+        self.faction.set("Republic")
+        self.gui_profile.set("Select")
+        for intvar in self.imp_ship_variables.values():
+            intvar.set(0)
+        for intvar in self.rep_ship_variables.values():
+            intvar.set(0)
