dc3486a7c4da2c9b413fbde5157c57caa568b9ea
RedFantom <redfantom@outlook.com>
2017-09-25 08:57:15 +0200

    Add pseudo-code to make progess easier

--- parsing/flytext.py
+++ parsing/flytext.py
@@ -38,7 +38,10 @@ class FlyTextParser(object):
         self.on_target_template = Image.open(template_path).convert("RGB")
         self.ship = None
         self.firing = False
-        self.last_shot = None
+        self.last_shot = datetime.now()
+        self.primary = True
+        self.primary_keys = {True: "PrimaryWeapon", False: "PrimaryWeapon2"}
+        self.active_id = ""
 
     def update(self, screenshot=None, cursor_position=None):
         """
@@ -51,24 +54,65 @@ class FlyTextParser(object):
             x, y, pressed = self.mouse_queue.get()
             print("Mouse button was {}".format(pressed))
             self.firing = pressed
-        if screenshot is not None and cursor_position is not None and self.firing is True:
-            print("Updating FlyText")
+        if screenshot is not None and cursor_position is not None: # and self.firing is True:
             # (datetime.now() - self.last_shot).seconds < self.ship.components["PrimaryWeapon"]["FiringRate"]
             if not self.on_target(screenshot, cursor_position):
                 self.overlays.append(FlyText(self.master, cursor_position))
                 print("New FlyText opened for miss")
             else:
-                self.internal_queue.put(datetime.now())
-        while not self.line_queue.empty():
-            # Events are coming in, yay!
-            line, active_id = self.line_queue.get()
-            if line["source"] != active_id:
-                continue
-
+                self.internal_queue.put((datetime.now(), cursor_position))
+                print("Waiting for the result of shot made at {}".format(datetime.now().strftime("%H:%M:%S")))
+        self.update_evade()
         for overlay in self.overlays:
             self.overlays.remove(overlay) if overlay.alive is False else None
 
+    def update_evade(self):
+        """
+        Process lines from the line_queue and datetimes in the internal_queue in order to determine whether a shot that
+        still has to be checked  has hit or was evaded. This is determined by checking the events in the line_queue
+        against the firing rate of the primary weapon.
+        """
+        while not self.line_queue.empty():
+            # Events are coming in, yay!
+            results = self.line_queue.get()
+            if "ship" in results:
+                # ("ship", parsing.ship.Ship())
+                self.ship = results[1]
+                continue
+            line, active_id = results
+            # The FlyTextParser must keep track of the active id in order to be able to reset the PrimaryWeapon key
+            # if a new ship event comes in too late for the parser to process it before a shot is again processed
+            if active_id != self.active_id:
+                self.active_id = active_id
+                self.primary = True
+            if "Primary Weapon Swap" in line["ability"]:
+                self.primary = not self.primary
+                continue
+            if line["source"] != active_id:
+                continue
+            # event_time = datetime.strptime(line["time"], "[%H:%M:%S])
+            # if self.ship is None: break
+            # merge date of today into event_time
+            # waiting_for_result, cursor_position = self.internal_queue.get() if self.internal_queue.empty() \
+                # is False else None, None
+            # if waiting_for_result is None:
+                # empty the line_queue
+                # break
+            # delta = (event_time - waiting_for_event).seconds
+            # if (delta > -0.1 and delta < self.ship.components[self.primary_keys[self.primary]]["FiringRate"]:
+            #   shot landed
+            # else:
+            #   self.overlays.append(FlyText(self.master, cursor_position, text="Evade")
+        return
+
     def on_target(self, screenshot, cursor_position):
+        """
+        Determine whether a shot was made on-target by matching the targeting pointer in the position of the mouse
+        (using an automatically cropped screenshot based on a box calculated with the mouse position) to a template
+        stored in the assets folder (self.on_target_template). Return True if shot was made on-target, False if not.
+
+        In order to fine-tune this process, the maximum similarity score as well as the box value should be adjusted.
+        """
         if isinstance(screenshot, ScreenShot):
             screenshot = Image.frombytes("RGB", screenshot.size, screenshot.rgb)
         x, y = cursor_position
@@ -96,7 +140,10 @@ class FlyTextParser(object):
         self.mouse_queue.put((x, y, pressed))
 
     def close(self):
-        pass
+        for overlay in self.overlays:
+            if overlay.alive:
+                overlay.destroy()
+        return
 
     def __exit__(self):
         self.close()
@@ -120,12 +167,14 @@ class FlyText(Overlay):
                                                                          "size": font_size,
                                                                          "bold": bold,
                                                                          "italic": False},
-                         wait_time=20)
+                         wait_time=20, auto_init=False)
         self.original_position = position
         self._fadeout = fadeout
         self._fadetime = fadetime
         self._move_downward = move_downward
-        self.master.after(20, self.update_attributes)
+        self.master.after(0, self.initialize_window)
+        self.master.after(self._wait_time, self.update_attributes)
+        self.master.after(self._wait_time, self.update)
         self.alive = True
 
     def update_attributes(self):
@@ -142,13 +191,13 @@ class FlyText(Overlay):
             return
         self._opacity = opacity
         self.count += 1
-        self.master.after(20, self.update_attributes)
+        self.master.after(self._wait_time, self.update_attributes)
 
 
 if __name__ == '__main__':
     root = tk.Tk()
     line_queue = Queue()
-    line_queue.put({"ability": "Burst Laser Cannon"})
+    line_queue.put(({"ability": "Burst Laser Cannon", "source": "1"}, "1"))
     parser = FlyTextParser(root, line_queue)
     screenshot = Image.open("../assets/vision/test_flytext.png")
     parser.update(screenshot, (952, 578))
