104ded5c4ec88af2ff1837a2bb3cf39ba232b5a5
RedFantom <redfantom@outlook.com>
2017-10-08 12:50:12 +0200

    Add basic menus for ShipStatsToplevel

--- toplevels/shipstatstoplevel.py
+++ toplevels/shipstatstoplevel.py
@@ -21,36 +21,112 @@ class ShipStatsToplevel(tk.Toplevel):
     A Toplevel that shows the statistics for a certain ship in an organized manner, categorized and in alphabetical
     order where applicable, in human readable format.
     """
+
     def __init__(self, master, ship, ships_data, companions_data):
-        tk.Toplevel.__init__(self, master)
-        stats = ShipStats(ship, ships_data, companions_data)
+        """
+        :param master: MainWindow
+        :param ship: Ship instance to calculate statistics for
+        :param ships_data: Ships data dictionary (ships.db)
+        :param companions_data: Companions data dictionary (companions.db)
+        """
+        tk.Toplevel.__init__(self, master, width=350, height=400)
+        self.resizable(False, False)
+        self.stats = ShipStats(ship, ships_data, companions_data)
+        self.title("GSF Parser: Statistics for {}".format(ship.name))
+
+        # Create the Treeview and its scrollbar
         self.stats_scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
         self.stats_treeview = ttk.Treeview(self, show=("headings", "tree"), columns=("value",),
                                            yscrollcommand=self.stats_scrollbar.set)
         self.stats_scrollbar.config(command=self.stats_treeview.yview)
-        self.stats_treeview.heading("#0", text="Statistic")
-        self.stats_treeview.heading("value", text="Value")
-        self.stats_treeview.column("value", anchor=tk.E, width=50)
-        self.stats_treeview.column("#0", anchor=tk.W, width=350)
-        self.stats_treeview.insert("", tk.END, iid="Ship", text="Ship statistics")
-        self.stats_treeview.tag_configure("even", background="lightgrey")
-        tags = ("even", )
-        for item in sorted(stats.stats.keys()):
+        self.setup_treeview()
+
+        # Create main menu objects
+        self.main_menu = tk.Menu(self, tearoff=False)
+        self.file_menu = tk.Menu(self.main_menu, tearoff=False)
+        self.stats_menu = tk.Menu(self.main_menu, tearoff=False)
+        self.ttk_menu = tk.Menu(self.main_menu, tearoff=False)
+        self.excel_menu = tk.Menu(self.main_menu, tearoff=False)
+        self.setup_menu()
+
+        self.bind("<Configure>", self.configure)
+        self.update_tree()
+        self.grid_widgets()
+
+    def update_tree(self):
+        """
+        Update the Treeview with the data in the ShipStats instance
+        """
+        self.stats_treeview.delete(*self.stats_treeview.get_children())
+        self.stats_treeview.insert("", tk.END, iid="Ship", text="Ship statistics", tags=("category",))
+        tags = ("even",)
+        for item in sorted(self.stats.stats.keys()):
             if item.isdigit() or "OBSOLETE" in item:
                 continue
-            value = "{.0f}".format(round(stats.stats[item], 0))
+            value = self.stats.stats[item]
+            # value = value * 100 if value <= 2 else value
+            value = "{:.2f}".format(value)
             item = item.replace("_", " ")
             self.stats_treeview.insert("Ship", tk.END, iid=item, values=value, text=item, tags=tags)
-            tags = ("odd", ) if tags == ("even", ) else ("even", )
-        for component in stats.components:
-            pass
-        self.bind("<Configure>", self.configure)
-        self.grid_widgets()
+            tags = ("odd",) if tags == ("even",) else ("even",)
+        for component in self.stats.components:
+            print("Component: {}".format(component))
 
     def grid_widgets(self):
         self.stats_treeview.grid(row=1, column=1, sticky="nswe", padx=5, pady=5)
         self.stats_scrollbar.grid(row=1, column=2, sticky="ns", padx=(0, 5), pady=5)
 
-    def configure(self, *args):
-        self.grid_widgets()
+    def grid_widgets_forget(self):
+        self.stats_treeview.grid_forget()
+        self.stats_scrollbar.grid_forget()
 
+    def setup_menu(self):
+        """
+        Sets up the menu of the window
+        """
+        # Setup the File menu
+        self.file_menu.add_command(label="Export", command=self.export_ship)
+        self.file_menu.add_command(label="Import", command=self.import_ship)
+        self.file_menu.add_separator()
+        self.file_menu.add_command(label="Close", command=self.destroy)
+        # Setup the stats menu
+        self.stats_menu.add_command(label="Distance travelled", command=lambda: self.calculate_stat("distance"))
+        # Setup the TTK menu
+        self.ttk_menu.add_command(label="TTK against self", command=lambda: self.calculate_stat("ttk_self"))
+        self.ttk_menu.add_command(label="TTK against target", command=lambda: self.calculate_stat("ttk_target"))
+        self.stats_menu.add_cascade(label="Time-to-kill", menu=self.ttk_menu)
+        # Setup the Excel menu
+        pass
+        # Add the menus to the main_menu
+        self.main_menu.add_cascade(label="File", menu=self.file_menu)
+        self.main_menu.add_cascade(label="Stats", menu=self.stats_menu)
+        # Add the menu to the window
+        self.config(menu=self.main_menu)
+
+    def setup_treeview(self):
+        """
+        Sets up the Treeview with columns and headings
+        """
+        self.stats_treeview.heading("#0", text="Statistic")
+        self.stats_treeview.heading("value", text="Value")
+        self.stats_treeview.column("value", anchor=tk.E, width=50)
+        self.stats_treeview.column("#0", anchor=tk.W, width=350)
+        self.stats_treeview.tag_configure("category", background="", font=("default", 12, "bold"))
+        self.stats_treeview.tag_configure("even", background="lightgrey")
+
+    def configure(self, *args):
+        pass
+
+    def export_ship(self):
+        pass
+
+    def import_ship(self):
+        pass
+
+    def calculate_stat(self, stat):
+        pass
+
+
+class StatsCalculationToplevel(tk.Toplevel):
+    def __init__(self, *args, **kwargs):
+        tk.Toplevel.__init__(self, *args, **kwargs)
