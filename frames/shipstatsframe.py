e245311670387f8b896809e34e4defc0ea66dad7
RedFantom <redfantom@outlook.com>
2017-11-14 19:50:48 +0100

    Update ShipStatsFrame to work with new calculations

--- frames/shipstatsframe.py
+++ frames/shipstatsframe.py
@@ -7,14 +7,13 @@
 # UI imports
 import tkinter as tk
 import tkinter.ttk as ttk
-import tkinter.messagebox
-import os
-import sys
+# General imports
+import textwrap
 # Own modules
-from tools import utilities
 from parsing.shipstats import ShipStats
 from parsing.ships import component_types_list, component_strings
-import variables
+from parsing.statistics import categories as statistic_categories, statistics as statistic_strings
+from parsing.statistics import weapon_categories as weapon_statistic_categories
 
 
 class ShipStatsFrame(ttk.Frame):
@@ -32,6 +31,9 @@ class ShipStatsFrame(ttk.Frame):
         """
         ttk.Frame.__init__(self, master)
         self.stats = ShipStats(ship, ships_data, companions_data)
+        self.tags = {}
+        self.tags.update({key: "odd" for key in statistic_categories})
+        self.tags.update({key: "odd" for key in weapon_statistic_categories})
 
         # Create the Treeview and its scrollbar
         self.stats_scrollbar = ttk.Scrollbar(self, orient=tk.VERTICAL)
@@ -40,7 +42,6 @@ class ShipStatsFrame(ttk.Frame):
         self.stats_scrollbar.config(command=self.stats_treeview.yview)
         self.setup_treeview()
 
-        self.bind("<Configure>", self.configure)
         self.update_tree()
         self.grid_widgets()
 
@@ -49,32 +50,55 @@ class ShipStatsFrame(ttk.Frame):
         Update the Treeview with the data in the ShipStats instance
         """
         self.stats_treeview.delete(*self.stats_treeview.get_children())
-        self.stats_treeview.insert("", tk.END, iid="Ship", text="Ship statistics", tags=("category",))
-        tags = ("even",)
-        for item in sorted(self.stats.stats.keys()):
-            if item.isdigit() or "OBSOLETE" in item:
+        # First insert the header items for Ship statistics
+        self.stats_treeview.insert("", tk.END, iid="Ship", tags=("category",), text="Ship")
+        for category in statistic_categories:
+            self.stats_treeview.insert(
+                "Ship", tk.END, tags=("stat_category",), text=category.replace("_", " "), iid="Ship_{}".format(category)
+            )
+        # Insert the Ship statistics themselves
+        for statistic, value in sorted(self.stats["Ship"].items()):
+            if statistic not in statistic_strings:
                 continue
-            value = self.stats.stats[item]
-            # value = value * 100 if value <= 2 else value
-            value = "{:.2f}".format(value)
-            item = item.replace("_", " ")
-            self.stats_treeview.insert("Ship", tk.END, iid=item, values=value, text=item, tags=tags)
-            tags = ("odd",) if tags == ("even",) else ("even",)
-        for component in component_types_list:
-            if component not in self.stats.components:
+            self.insert_into_treeview("Ship", statistic, value)
+        # Loop over the other categories to insert the other statistics
+        for category in component_types_list:
+            # Skip any categories that are not in the ShipStats object
+            if category not in self.stats:
                 continue
-            print("Component: {}, {}".format(component, self.stats.components[component]))
-            string = component_strings[component] if component in component_strings else component
-            self.stats_treeview.insert("", tk.END, iid=component, text=string, tags=("category",))
-            for key in sorted(self.stats.components[component].keys()):
-                print("Processing component stat {}".format(key))
-                if "OBSOLETE" in key or "[Pc]" in key:
-                    continue
-                value = "{:.2f}".format(self.stats.components[component][key])
-                key = key.replace("_", " ")
-                self.stats_treeview.insert(component, tk.END, iid="{}_{}".format(component, key), text=key, tags=tags,
-                                           values=value)
-                tags = ("odd",) if tags == ("even",) else ("even",)
+            # Empty categories should not be inserted
+            stats = self.stats[category].items()
+            if stats is None or len(stats) == 0:
+                continue
+            # Insert the header for the category
+            self.stats_treeview.insert("", tk.END, iid=category, tags=("category",), text=component_strings[category])
+            # insert the headers for the subcategories
+            for subcategory in weapon_statistic_categories:
+                self.stats_treeview.insert(
+                    category, tk.END, iid="{}_{}".format(category, subcategory),
+                    tags=("stat_category",), text=subcategory
+                )
+            for statistic, value in sorted(stats):
+                self.insert_into_treeview(category, statistic, value)
+        return
+
+    def insert_into_treeview(self, category, statistic, value):
+        """
+        Insert a statistic into the treeview. Parent categories should already be in place.
+        """
+        subcategory = statistic_strings[statistic][0]
+        if category == "Ship" and subcategory not in statistic_categories:
+            return
+        tag = self.tags[subcategory]
+        value_string = ShipStatsFrame.get_value_string(statistic, value)
+        elements = textwrap.fill(statistic_strings[statistic][1], 25).split("\n")
+        for index, element in enumerate(elements):
+            print("Inserting element for {}, {}, {}, {}".format(category, statistic, value, tag))
+            self.stats_treeview.insert(
+                "{}_{}".format(category, subcategory), tk.END, tags=(tag,), text=element,
+                values=("",) if index != 0 else (value_string,)
+            )
+        self.tags[subcategory] = "even" if self.tags[subcategory] == "odd" else "odd"
 
     def grid_widgets(self):
         self.stats_treeview.grid(row=1, column=1, sticky="nswe", padx=5, pady=5)
@@ -90,9 +114,28 @@ class ShipStatsFrame(ttk.Frame):
         """
         self.stats_treeview.heading("#0", text="Statistic")
         self.stats_treeview.heading("value", text="Value")
-        self.stats_treeview.column("value", anchor=tk.E, width=40)
-        self.stats_treeview.column("#0", anchor=tk.W, width=220)
-        self.stats_treeview.tag_configure("category", background="grey90", font=("default", 12, "bold"))
+        self.stats_treeview.column("value", anchor=tk.E, width=55)
+        self.stats_treeview.column("#0", anchor=tk.W, width=205)
+        self.stats_treeview.tag_configure("category", background="gray90", font=("default", 12, "bold"))
+        self.stats_treeview.tag_configure("stat_category", background="gray85", font=("default", 11, "italic"))
         self.stats_treeview.tag_configure("even", background="gray80")
 
     @staticmethod
+    def get_value_string(statistic, value):
+        """
+        Get a nicely formatted string to insert into the value column of the Treeview based on the unit
+        """
+        category, string, unit = statistic_strings[statistic]
+        if "%" in unit:
+            value_string = "{:.1f}{}".format(value * 100, unit)
+        elif unit == "bool":
+            value_string = "False" if value == 0.0 else "True"
+        elif unit == "p":
+            value_string = "{:.0f} {}".format(value, unit)
+        elif "m/s" in unit:
+            value_string = "{:.1f} {}".format(value * 10, unit)
+        elif unit == "m":
+            value_string = "{} {}".format(value * 100, unit)
+        else:
+            value_string = "{:.1f} {}".format(value, unit)
+        return textwrap.fill(value_string, 25)
