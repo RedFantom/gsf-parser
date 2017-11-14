51a0b9b4bbc55367d2b5e4c4e11c071c721e35ed
RedFantom <redfantom@outlook.com>
2017-11-14 10:29:49 +0100

    Add Cluster Missiles to durations file

--- parsing/durations.py
+++ parsing/durations.py
@@ -20,7 +20,8 @@ durations = {
     "Running Interference": (True, 15, False),
     "Melting Hull": (False, 4, True),
     "Plasma Burn": (False, 6, True),
-    "Plasma Warheads": (False, 10, True)
+    "Plasma Warheads": (False, 10, True),
+    "Cluster Missiles": (False, 15, True)
 }
 
 special_cases = ["EMP Missile", "EMP Field", "Plasma Warheads"]
