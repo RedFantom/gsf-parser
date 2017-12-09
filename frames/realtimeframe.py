d322e72fe5d4039072a8057bf48202207973d91f
RedFantom <redfantom@outlook.com>
2017-12-09 11:38:06 +0100

    Merge optimizations from overlays into parser

--- frames/realtimeframe.py
+++ frames/realtimeframe.py
@@ -207,38 +207,28 @@ class RealtimeFrame(ttk.Frame):
                                            str(selfdamage) + "\n" +
                                            str(healing) + "\n" +
                                            str(spawns))
-        if self.overlay:
-            if variables.settings_obj["realtime"]["size"] == "big":
-                self.overlay.stats_var.set(str(damage_done) + "\n" +
-                                           str(damage_taken) + "\n" +
-                                           str(healing) + "\n" +
-                                           str(selfdamage) + "\n" +
-                                           str(enemies) + "\n" +
-                                           str(spawns))
-            elif variables.settings_obj["realtime"]["size"] == "small":
-                self.overlay.stats_var.set(str(damage_done) + "\n" +
-                                           str(damage_taken) + "\n" +
-                                           str(healing) + "\n" +
-                                           str(selfdamage) + "\n")
-            else:
-                raise ValueError("Not a valid overlay size found.")
+        if self.overlay is not None:
+            size = variables.settings_obj["realtime"]["size"]
+            args = ((damage_done, damage_taken, selfdamage, healing) if size == "small" else
+                    (damage_done, damage_taken, selfdamage, healing, spawns, enemies))
+            self.overlay.text_var.set(self.overlay.unformatted_string.format(*args))
 
     def callback(self, lines):
         if not self.parsing:
             return
         for line in lines:
             # self.listbox.see(tk.END)
-            process = realtime.Parser.line_to_dictionary(line)
+            process = lineops.line_to_dictionary(line)
             self.parser.parse(process)
-            dmg_done = self.parser.spawn_dmg_done
-            dmg_taken = self.parser.spawn_dmg_taken
-            selfdamage = self.parser.spawn_selfdmg
-            healing = self.parser.spawn_healing_rcvd
-            abilities = self.parser.tmp_abilities
-            enemies = self.parser.recent_enemies
-            spawns = self.parser.active_ids
-            self.update_stats(dmg_done, dmg_taken, selfdamage, healing, abilities,
-                              len(enemies), len(spawns))
+        dmg_done = self.parser.spawn_dmg_done
+        dmg_taken = self.parser.spawn_dmg_taken
+        selfdamage = self.parser.spawn_selfdmg
+        healing = self.parser.spawn_healing_rcvd
+        abilities = self.parser.tmp_abilities
+        enemies = self.parser.recent_enemies
+        spawns = self.parser.active_ids
+        self.after(10, lambda: self.update_stats(dmg_done, dmg_taken, selfdamage, healing, abilities,
+                               len(enemies), len(spawns)))
         for obj in self.parse:
             obj.close()
 
