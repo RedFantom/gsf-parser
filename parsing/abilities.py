c3e18045ca36a3ed139f560f1fc34d7df46811ac
RedFantom <redfantom@outlook.com>
2018-01-12 19:55:26 +0100

    Refactor abilities.py

--- parsing/abilities.py
+++ parsing/abilities.py
@@ -3,107 +3,103 @@ Author: RedFantom
 Contributors: Daethyra (Naiii) and Sprigellania (Zarainia)
 License: GNU GPLv3 as in LICENSE
 Copyright (C) 2016-2018 RedFantom
-"""
 
-"""
-This file contains lists and dictionaries of the abilities and ships used in Galactic StarFighter,
-in order for the parser to be able to identify ships by their components and abilities and print
-them neatly onto the screen.
+
+This file contains lists and dictionaries of the abilities and ships
+used in Galactic StarFighter, in order for the parser to be able to
+identify ships by their components and abilities and print them neatly
+onto the screen.
 """
 from collections import OrderedDict
-
-# A list of ALL components available in Galactic StarFighter
-components = ["Heavy Laser Cannon", "Laser Cannon", "Light Laser Cannon", "Quad Laser Cannon",
-              "Rapid-fire Laser Cannon", "Ion Cannon", "Burst Laser Cannon",
-              "Proton Torpedo", "Concussion Missile", "Thermite Torpedo", "Cluster Missiles", "Seeker Mine",
-              "Rocket Pod", "EMP Missile", "Ion Missile",
-              "Sabotage Probe", "Seismic Mine", "Slug Railgun", "Ion Railgun", "Plasma Railgun", "Interdiction Missile",
-              "Shield Power Converter", "Weapon Power Converter", "Engine Power Converter", "Rotational Thrusters",
-              "Koiogran Turn", "Snap Turn", "Retro Thrusters",
-              "Power Dive", "Interdiction Drive", "Hyperspace Beacon", "Barrel Roll",
-              "Railgun Sentry Drone", "Interdiction Sentry Drone", "Missile Sentry Drone", "Targeting Telemetry",
-              "Blaster Overcharge", "Booster Recharge",
-              "Combat Command", "Repair Probes", "Remote Slicing", "Sensor Beacon", "EMP Field", "Interdiction Mine",
-              "Ion Mine", "Concussion Mine", "Tensor Field",
-              "Charged Plating", "Overcharged Shield", "Shield Projector", "Repair Drone", "Overcharged Shield",
-              "Fortress Shield", "Feedback Shield",
-              "Directional Shield", "Distortion Field", "Quick-Charge Shield"]
-
-component_types = ["primaries", "secondaries", "engines", "shields", "systems"]
-
-# The ID numbers of abilities and their English names
-# For future support of multiple Combatlog languages
-# Automatically generated, some abilities may be missing
-components_english = {'3330936116609024': 'Hyperspace Beacon',
-                      '3300630827368448': 'Shield Power Converter',
-                      '3294609283219456': 'Distortion Field',
-                      '3295356607528960': 'Retro Thrusters',
-                      '3294897046028288': 'Laser Cannon',
-                      '3320048374513664': 'Snap Turn',
-                      '3362078924472320': 'EMP Field',
-                      '3295768924389637': 'Booster Recharge',
-                      '3302232850169856': 'Repair Drone',
-                      '3290932791214080': 'Light Laser Cannon',
-                      '3290958561017856': 'Burst Laser Cannon',
-                      '3298831236071424': 'Cluster Missiles',
-                      '3301777583636480': 'Slug Railgun',
-                      '3323312549658624': 'Concussion Mine',
-                      '3397954786295808': 'Interdiction Missile',
-                      '3292272821010432': 'Quad Laser Cannon',
-                      '3295305067921408': 'Rocket Pod',
-                      '3298985854894080': 'Proton Torpedo',
-                      '3301949382328320': 'Feedback Shield',
-                      '3298981559926784': 'Ion Missile',
-                      '3296133996609536': 'Sabotage Probe',
-                      '3321744886595584': 'Remote Slicing',
-                      '3300639417303040': 'Overcharged Shield',
-                      '3295313657856000': 'Barrel Roll',
-                      '3292320065650688': 'Targeting Telemetry',
-                      '811980747177984': 'Charged Plating',
-                      '3321878030581760': 'Interdiction Drive',
-                      '3325030536577024': 'Seismic Mine',
-                      '3328019833815040': 'Railgun Sentry Drone',
-                      '3300759676387328': 'Combat Command',
-                      '3357483309465600': 'EMP Missile',
-                      '3302542087815168': 'Interdiction Sentry Drone',
-                      '3295193398771712': 'Directional Shield',
-                      '3324609629782016': 'Seeker Mine',
-                      '3300751086452736': 'Repair Probes',
-                      '3301786173571072': 'Ion Railgun',
-                      '3295317952823296': 'Power Dive',
-                      '3295309362888704': 'Koiogran Turn',
-                      '3295425327005696': 'Tensor Field',
-                      '3290962855985152': 'Heavy Laser Cannon',
-                      '3298960085090304': 'Concussion Missile',
-                      '3325073486249984': 'Thermite Torpedo',
-                      '3301794763505664': 'Plasma Railgun',
-                      '3311600173842432': 'Fortress Shield',
-                      '3300703841812480': 'Shield Projector',
-                      '3327289689374720': 'Interdiction Mine',
-                      '3326061328728064': 'Ion Mine'}
 
 """
 All components sorted into categories of their component type
 """
-primaries = ["Heavy Laser Cannon", "Laser Cannon", "Light Laser Cannon", "Quad Laser Cannon", "Rapid-fire Laser Cannon",
-             "Ion Cannon", "Burst Laser Cannon"]
-secondaries = ["Proton Torpedo", "Concussion Missile", "Thermite Torpedo", "Cluster Missiles", "Seeker Mine",
-               "Rocket Pod", "EMP Missile", "Ion Missile",
-               "Sabotage Probe", "Seismic Mine", "Slug Railgun", "Ion Railgun", "Plasma Railgun",
-               "Interdiction Missile", "Rocket Pods"]
-               # The reference to Rocket Pods (plural) is for the build calculator
-engines = ["Shield Power Converter", "Weapon Power Converter", "Engine Power Converter", "Rotational Thrusters",
-           "Koiogran Turn", "Snap Turn", "Retro Thrusters",
-           "Power Dive", "Interdiction Drive", "Hyperspace Beacon", "Barrel Roll"]
-systems = ["Railgun Sentry Drone", "Interdiction Sentry Drone", "Missile Sentry Drone", "Targeting Telemetry",
-           "Blaster Overcharge", "Booster Recharge",
-           "Combat Command", "Repair Probes", "Remote Slicing", "Sensor Beacon", "EMP Field", "Interdiction Mine",
-           "Ion Mine", "Concussion Mine", "Tensor Field"]
-shields = ["Charged Plating", "Overcharged Shield", "Shield Projector", "Repair Drone",
-           "Fortress Shield", "Feedback Shield",
-           "Directional Shield", "Distortion Field", "Quick-Charge Shield"]
-copilots = ["Lockdown", "Slicer's Loop", "Servo Jammer", "Wingman", "Lingering Effect", "Hydro Spanner", "Nullify",
-            "Concentrated Fire", "In Your Sights", "Running Interference", "Suppression", "Bypass"]
+primaries = (  # PrimaryWeapon
+    "Heavy Laser Cannon",
+    "Laser Cannon",
+    "Light Laser Cannon",
+    "Quad Laser Cannon",
+    "Rapid-fire Laser Cannon",
+    "Ion Cannon",
+    "Burst Laser Cannon",
+)
+secondaries = (  # SecondaryWeapon
+    "Proton Torpedo",
+    "Concussion Missile",
+    "Thermite Torpedo",
+    "Cluster Missiles",
+    "Seeker Mine",
+    "Rocket Pod",
+    "EMP Missile",
+    "Ion Missile",
+    "Sabotage Probe",
+    "Seismic Mine",
+    "Slug Railgun",
+    "Ion Railgun",
+    "Plasma Railgun",
+    "Interdiction Missile",
+    # The ability name is Rocket Pod, the component name is Rocket Pods
+    "Rocket Pods",
+)
+engines = (  # Engine
+    "Shield Power Converter",
+    "Weapon Power Converter",
+    "Engine Power Converter",
+    "Rotational Thrusters",
+    "Koiogran Turn",
+    "Snap Turn",
+    "Retro Thrusters",
+    "Power Dive",
+    "Interdiction Drive",
+    "Hyperspace Beacon",
+    "Barrel Roll",
+)
+systems = (  # Systems
+    "Railgun Sentry Drone",
+    "Interdiction Sentry Drone",
+    "Missile Sentry Drone",
+    "Targeting Telemetry",
+    "Blaster Overcharge",
+    "Booster Recharge",
+    "Combat Command",
+    "Repair Probes",
+    "Remote Slicing",
+    "Sensor Beacon",
+    "EMP Field",
+    "Interdiction Mine",
+    "Ion Mine",
+    "Concussion Mine",
+    "Tensor Field"
+)
+shields = (  # Shield
+    "Charged Plating",
+    "Overcharged Shield",
+    "Shield Projector",
+    "Repair Drone",
+    "Fortress Shield",
+    "Feedback Shield",
+    "Directional Shield",
+    "Distortion Field",
+    "Quick-Charge Shield"
+)
+copilots = (  # CoPilot active abilities
+    "Lockdown",
+    "Slicer's Loop",
+    "Servo Jammer",
+    "Wingman",
+    "Lingering Effect",
+    "Hydro Spanner",
+    "Nullify",
+    "Concentrated Fire",
+    "In Your Sights",
+    "Running Interference",
+    "Suppression",
+    "Bypass",
+)
+
+# A tuple of all active components in Galactic StarFighter
+components = primaries + secondaries + engines + shields + systems
 
 """
 These lists were made with the help of Yellowbird
@@ -114,8 +110,8 @@ legionAbilities = [
     "Heavy Laser Cannon", "Laser Cannon", "Light Laser Cannon",
     "Proton Torpedo", "Concussion Missile", "Seeker Mine",
     "Shield Power Converter", "Interdiction Drive",
-    "Railgun", "Railgun Sentry Drone", "Interdiction Sentry Drone", "Missile Sentry Drone",
-    # Railgun = Railgun Sentry Drone
+    "Railgun", "Railgun Sentry Drone",  # Railgun = Railgun Sentry Drone
+        "Interdiction Sentry Drone", "Missile Sentry Drone",
     "Shield Projector", "Repair Drone", "Overcharged Shield"
 ]
 razorwireAbilities = [
@@ -192,6 +188,8 @@ quellAbilities = [
     "Weapon Power Converter", "Shield Power Converter", "Koiogran Turn", "Barrel Roll",
     "Quick-Charge Shield", "Directional Shield", "Charged Plating"
 ]
+
+# Abilities that are excluded from parsing
 excluded_abilities = [
     "Wingman", "Hydro Spanner", "In Your Sights", "Slicer's Loop",
     "Servo Jammer", "Lockdown", "Concentrated Fire", "Lingering Effect",
