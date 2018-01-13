f710ecbd0b863c5ad1f1aff486dc4d8f4121de3c
RedFantom <redfantom@outlook.com>
2018-01-13 13:24:55 +0100

    Remove deprecated code

--- main.py
+++ main.py
@@ -4,44 +4,35 @@ Contributors: Daethyra (Naiii) and Sprigellania (Zarainia)
 License: GNU GPLv3 as in LICENSE
 Copyright (C) 2016-2018 RedFantom
 """
-
+from tkinter import messagebox, filedialog
 from os.path import dirname, join, basename, exists
-from os import rmdir
 import sys
 import shutil
 import platform
-from tkinter import messagebox, filedialog
-from utils.directories import get_temp_directory
 
 
-def new_window():
+def create_window():
+    """
+    Attempt to create a new MainWindow and provide error handling and
+    user interaction for when an error occurs which causes the
+    window creation to fail.
+    """
     import gui
     try:
         main_window = gui.MainWindow()
     except Exception as e:
         if exists("development"):
             raise
-        save = messagebox.askyesno("Error", "The GSF Parser window failed to correctly initialize. Please report this "
-                                            "error along with the debug output below.\n\n{}: {}\n\nWould you like to "
-                                            "save the debug output to a file?".format(type(e), e))
+        save = messagebox.askyesno(
+            "Error", "The GSF Parser window failed to correctly initialize. Please report this "
+                     "error along with the debug output below.\n\n{}\n\nWould you like to "
+                     "save the debug output to a file?".format(repr(e), e))
         if save is True:
             filename = filedialog.asksaveasfilename()
             if filename is None:
                 raise ValueError("Invalid filename provided")
             with open(filename, "w") as fo:
-                fo.write(str(e))
-        clear = messagebox.askyesno(
-            "Question",
-            "The GSF Parser can automatically delete all your temporary data. This may solve your issue if it is "
-            "caused by incompatibility of an old data file with the expected data in a new one.\n\n"
-            "WARNING: This process WILL delete your characters, builds and real-time parsing data if you had any.\n\n"
-            "Do you want to attempt the automatic fix?"
-        )
-        if clear is True:
-            try:
-                rmdir(get_temp_directory())
-            except OSError:
-                messagebox.showerror("Error", "Could not automatically delete the temporary files.")
+                fo.write(repr(e))
         raise
     try:
         main_window.mainloop()
@@ -50,6 +41,9 @@ def new_window():
 
 
 def setup_tkinter():
+    """
+    Fixes bug in PyInstaller, see issue #17.
+    """
     if "Windows-7" not in platform.platform():
         return
     try:
@@ -63,4 +57,4 @@ def setup_tkinter():
 
 if __name__ == '__main__':
     setup_tkinter()
-    new_window()
+    create_window()
