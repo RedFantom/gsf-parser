6011a08882ac14907da1994feb87b443de7cc74d
RedFantom <redfantom@outlook.com>
2018-04-02 11:42:19 +0200

    Remove network/__init__.py imports
    The imports caused a cascade of imports on a Linux server platform that should not require all the requirements the GSF Parser requires. Tkinter, for example, should not have to be installed.

--- network/__init__.py
+++ network/__init__.py
@@ -1,15 +1,8 @@
 """
 Author: RedFantom
 Contributors: Daethyra (Naiii) and Sprigellania (Zarainia)
-License: GNU GPLv3 as in LICENSE
+License: GNU GPLv3 as in LICENSE.md
 Copyright (C) 2016-2018 RedFantom
 """
-# MiniMap
-from network.minimap.server import MiniMapServer
-from network.minimap.client import MiniMapClient
-# Strategy
-from network.strategy.server import StrategyServer
-from network.strategy.client import StrategyClient
-# Sharing
-from network.sharing.server import SharingServer
-from network.sharing.client import SharingClient
+from .client import Client
+from .clienthandler import ClientHandler
