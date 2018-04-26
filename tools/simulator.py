45ddaf00c6b987a2453c3196fc6aae659d658473
RedFantom <redfantom@outlook.com>
2018-04-26 17:13:17 +0200

    Revert "Remvoe the Simulator and CartelFix tools"
    This reverts commit 8787da69adcfd3a5be11e7336c6bf250361b5245.

---
+++ tools/simulator.py
@@ -0,0 +1,51 @@
+﻿"""
+Author: RedFantom
+Contributors: Daethyra (Naiii) and Sprigellania (Zarainia)
+License: GNU GPLv3 as in LICENSE
+Copyright (C) 2016-2018 RedFantom
+"""
+from datetime import datetime
+import time
+import os
+import threading
+from queue import Queue
+
+
+class Simulator(threading.Thread):
+    """
+    Takes an existent log file and copies it into a newly created file,
+    simulating the time-delta.
+    """
+    def __init__(self, input_file, output_directory=None):
+        threading.Thread.__init__(self)
+        self.input_file = input_file
+        self.output_directory = output_directory
+        self.exit_queue = Queue()
+
+    def run(self):
+        # Create new log file name with the form: "combat_2012-04-17_20_08_37_242569.txt"
+        timestamp = str(datetime.now()).replace(" ", "_").replace(":", "_").replace(".", "_")
+        file_name = "combat_" + timestamp + ".txt"
+        last, current = "", ""
+        format = "%H:%M:%S.%f"
+
+        with open(self.input_file, "r") as fi, open(os.path.join(self.output_directory, file_name), "a") as fo:
+            for line in fi:
+
+                if not self.exit_queue.empty():
+                    break
+
+                if last is "":
+                    last = line.split(None, 1)[0].replace('[', '').replace(']', '')
+                else:
+                    last = current
+
+                current = line.split(None, 1)[0].replace('[', '').replace(']', '')
+
+                delta = datetime.strptime(current, format) - datetime.strptime(last, format)
+                delta_sec = delta.total_seconds()
+
+                fo.write(line)
+                fo.flush()
+                time.sleep(delta_sec)
+
---
+++ toplevels/cartelfix.py
@@ -0,0 +1,94 @@
+"""
+Author: RedFantom
+Contributors: Daethyra (Naiii) and Sprigellania (Zarainia)
+License: GNU GPLv3 as in LICENSE
+Copyright (C) 2016-2018 RedFantom
+"""
+
+# UI Libraries
+import tkinter as tk
+from tkinter.messagebox import showinfo
+# Standard Library
+from pynput import keyboard
+import sys
+# Project Modules
+import variables
+from utils import admin
+
+
+factions = {
+    "Imperial": "imp",
+    "Republic": "rep"
+}
+
+railguns = {
+    "Slug Railgun": {"imp": "03", "rep": "03"},
+    "Ion Railgun": {"imp": "02", "rep": "01"},
+    "Plasma Railgun": {"imp": "04", "rep": "04"}
+}
+
+
+class CartelFix(tk.Toplevel):
+    """
+    Provide a teensy overlay for Cartel Market Gunships secondary
+    weapon icons.
+    """
+    def __init__(self, master: tk.Tk, first: tk.PhotoImage, second: tk.PhotoImage, coordinates: tuple):
+        if not admin.check_privileges():
+            if sys.platform == "win32":
+                variables.main_window.destroy()
+                admin.escalate_privileges()
+                exit()
+            else:
+                showinfo("Information", "This feature is currently not supported on Unix machines.")
+                raise ValueError()
+        tk.Toplevel.__init__(self, master)
+        self.label = tk.Label(self)
+        self.railgun = 1
+        self.first = first
+        self.second = second
+        self.label.config(image=self.first)
+        self.label.grid()
+        self.listener = keyboard.Listener(on_press=self.switch)
+        self.coordinates = coordinates
+        self.set_geometry()
+
+    def switch(self, key: keyboard.Key):
+        """
+        Callback for the keyboard press Listener. Only if the pressed
+        key is `1` on the keyboard (either NumPad or numeric row)
+        will the icon switch status.
+        """
+        try:
+            if int(key.char) != 1:
+                return True
+        except AttributeError:
+            return True
+        except ValueError:
+            return True
+        if self.railgun == 1:
+            self.label.config(image=self.second)
+            self.railgun = 2
+        elif self.railgun == 2:
+            self.label.config(image=self.first)
+            self.railgun = 1
+        else:
+            raise ValueError("Not a valid railgun number: {0}".format(self.railgun))
+        return True
+
+    def set_geometry(self):
+        """Update window geometry and attributes"""
+        self.configure(background="white")
+        self.wm_attributes("-transparentcolor", "white")
+        self.overrideredirect(True)
+        self.attributes("-topmost", True)
+        self.wm_geometry("+{0}+{1}".format(self.coordinates[0], self.coordinates[1]))
+
+    def start_listener(self):
+        self.listener.start()
+
+    @staticmethod
+    def generate_icon_path(faction: str, railgun: str):
+        """Generate the filename for specified railgun for faction"""
+        base = "spvp.{}.gunship.sweapon.{}.jpg"
+        return base.format(factions[faction], railguns[railgun][factions[faction]])
