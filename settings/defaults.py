27a124424d81a4740d22d112d21dff6d9ca38286
RedFantom <redfantom@outlook.com>
2018-07-25 17:21:48 +0200

    Bump version number to v5.1.0

--- settings/defaults.py
+++ settings/defaults.py
@@ -13,7 +13,7 @@ defaults = {
     # Miscellaneous settings
     "misc": {
         # GSF Parser version number (used for update checks)
-        "version": "v5.0.0",
+        "version": "v5.1.0",
         # Whether checks for updates are enabled
         "autoupdate": True,
         # assets/ships.db SWTOR patch level
