35702ddcf759383856f756ca9311c91a2f8b2d7f
RedFantom <redfantom@outlook.com>
2018-08-14 21:34:30 +0200

    Implement Login event reading for files already created

--- parsing/logstalker.py
+++ parsing/logstalker.py
@@ -10,12 +10,12 @@ from parsing.parser import Parser
 import variables
 
 
-def sort_file(file_name: str) -> int:
+def sort_file(file_name: str) -> float:
     """Function to convert file name to time """
     r: datetime = Parser.parse_filename(file_name)
     if r is not None:
         return r.timestamp()
-    return 0
+    return 0.0
 
 
 class LogStalker(object):
@@ -30,6 +30,7 @@ class LogStalker(object):
         :param folder: Folder to watch CombatLogs in
         :param watching_callback: Callback to be called when the watched file changes
         """
+        self._new_file = False
         self._folder = folder
         self._watching_callback = watching_callback
         self.file = None
@@ -53,6 +54,7 @@ class LogStalker(object):
         self._read_so_far = 0
         self._watching_callback(self.file)
         self._process_new_file()
+        self._new_file = True
 
     def _process_new_file(self):
         """Backlog only the lines of a match that are match lines"""
@@ -80,19 +82,23 @@ class LogStalker(object):
     def get_new_lines(self):
         """Read the new lines in the file and return them as a list"""
         self.update_file()
-        dictionaries = self.read_file(self.path, self._read_so_far)
+        dictionaries = self.read_file(self.path, self._read_so_far, self._new_file)
+        self._new_file = False
         self._read_so_far += len(dictionaries)
         if None in dictionaries:
             raise ValueError()
         return dictionaries
 
     @staticmethod
-    def read_file(path, skip):
+    def read_file(path, skip, include=False):
         """Read the file in UnicodeDecodeError safe method"""
         with open(path, "rb") as fi:
-            lines = fi.readlines()[skip:]
+            lines = fi.readlines()
+        read = lines[skip:]
+        if include:
+            read.insert(0, lines[0])
         dictionaries = []
-        for line in lines:
+        for line in read:
             try:
                 line = line.decode()
             except UnicodeDecodeError:
